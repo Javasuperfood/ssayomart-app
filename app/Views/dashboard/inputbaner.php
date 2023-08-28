fd68d6240067bf144a03eb58d1dca12c737faad7
Gilang Aditia <gaditia744@gmail.com>
2023-08-28 16:58:47 +0700

    update button

--- app/Views/dashboard/inputbaner.php
+++ app/Views/dashboard/inputbaner.php
@@ -16,7 +16,9 @@
             </button>
             <!-- dialog modal -->
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dial
+                
+                og">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
