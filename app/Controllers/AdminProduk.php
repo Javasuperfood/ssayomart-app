c0de9233a6adabc91fd8bf791468c2d5ed6b0620
Gilang Aditia <gaditia744@gmail.com>
2023-08-31 17:01:57 +0700

    update crud

--- app/Controllers/AdminProduk.php
+++ app/Controllers/AdminProduk.php
@@ -17,27 +17,50 @@ class AdminProduk extends BaseController
 
     public function tambahProduk()
     {
-
+        $produkModel = new ProdukModel();
+        $produk_list = $produkModel->findAll();
         $data = [
-            'title' => 'menambahkan data produk'
+            'title' => 'produk',
+            'produk_Model' => $produk_list
         ];
         return view('dashboard/tambahProduk', $data);
     }
 
+    // save
     public function save()
     {
+
         // dd($this->request->getVar());
         $produkModel = new ProdukModel();
-        $slug = url_title($this->request->getVar('produk'), '-', true);
+        $slug = url_title($this->request->getVar('nama_produk'), '-', true);
         $data = [
             'slug' => $slug,
-            'nama_produk' => $this->request->getVar('produk'),
-            'harga_produk' => $this->request->getVar('harga'),
-            'deskripsi_produk' => $this->request->getVar('deskripsi'),
-            'stock_produk' => $this->request->getVar('stock'),
-            // 'gambar_produk' => $this->request->getVar('gambar'),
+            'nama' => $this->request->getVar('nama_produk'),
+            'harga' => $this->request->getVar('harga_produk'),
+            'deskripsi' => $this->request->getVar('deskripsi_produk'),
+            'stok' => $this->request->getVar('stock_produk'),
+            'img' => $this->request->getVar('gambar'),
         ];
-        // dd($data);
-        $produkModel->save($data);
+        // swet alert
+        if ($produkModel->save($data)) {
+            session()->setFlashdata('success', 'Prroduk berhasil disimpan.');
+            $alert = [
+                'type' => 'success',
+                'title' => 'Berhasil',
+                'message' => 'produk berhasil disimpan.'
+            ];
+            session()->setFlashdata('alert', $alert);
+
+            return redirect()->to('dashboard/tambah-produk');
+        } else {
+            $alert = [
+                'type' => 'error',
+                'title' => 'Error',
+                'message' => 'Terdapat kesalahan pada pengisian formulir'
+            ];
+            session()->setFlashdata('alert', $alert);
+
+            return redirect()->to('dashboard/tambah-produk')->withInput();
+        }
     }
 }
