ac46bf2ba5570fac9cf6d143fe9f5565c0c719d3
Gilang Aditia <gaditia744@gmail.com>
2023-08-31 08:52:09 +0700

    update layout

--- app/Views/dashboard/kategorisubkat.php
+++ app/Views/dashboard/kategorisubkat.php
@@ -1,221 +1,133 @@
 <?= $this->extend('dashboard/dashboard') ?>
 <?= $this->section('page-content') ?>
 
-<!-- Page Heading -->
-<!-- <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Informasi Barang</h1>
-</div> -->
+<h1 class="h3 mb-1 text-gray-800">Kategori Produk</h1>
+<p class="mb-4">Kategori produk untuk toko Anda dapat diatur di sini. Untuk mengganti urutan kategori di ujung depan, Anda dapat seret-lepas untuk mengurutkannya. Untuk melihat kategori lainnya klik tautan "Opsi Layar" pada bagian atas halaman.</p>
 
-<!-- <div class="container" >
-    <div class="row"> -->
-<!-- Earnings (Monthly) Card Example -->
-<!-- <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Produk</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">1200 Produk</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
+<div class="row">
+
+    <!-- Left Panel -->
+    <div class="col-lg-6">
+
+        <div class="card position-relative">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Form submit</h6>
             </div>
-        </div> -->
+            <div class="card-body">
+                <!-- code -->
+                <form action="/kategori/create">
 
-<!-- Earnings (Annual) Card Example -->
-<!-- <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Kategori</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">20 Kategori</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
+                    <div class="mb-3">
+                        <label for="kategori" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="kategori" placeholder="Nama Lengkap" name="kategori">
                     </div>
-                </div>
-            </div>
-        </div> -->
-
-
-<!-- Pending Requests Card Example -->
-<!-- <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Sub Kategori</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">30 Sub Kategori</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
+                    <div class="mb-3">
+                        <label for="slug" class="form-label">Slug</label>
+                        <input type="text" class="form-control" id="slug" placeholder="slug" name="slug">
                     </div>
-                </div>
+                    <div class="mb-1">
+                        <label for="kategori" class="form-label">Induk Kategori</label>
+                    </div>
+                    <div>
+                        <select class="form-select mb-2" aria-label="Default select example">
+                            <option selected>Pilihan</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <p class="mb-0 small">Note: Tetapkan sebuah istilah induk untuk membuat sebuah hirarki. Istilah Jazz, contohnya, akan menjadi induk dari Bebop dan Big Band.</p>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Deskripsi</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="small mb-1">Navbar Dropdown Example:</div>
+                    <p class="mb-0 small">Note: Umumnya deskripsi tidak tampil. Namun, beberapa tema dapat menampilkannya.</p>
+                    <div>
+                        <button type="submit" class="btn btn-primary mt-3" id="ka">Simpan</button>
+                    </div>
             </div>
         </div>
+        </form>
     </div>
-</div> -->
 
-<!-- Begin Page Content -->
-<div class="container-fluid">
+    <!-- Right Panel -->
+    <div class="col-lg-6">
 
-    <!-- Page Heading -->
-    <h1 class="h3 mb-1 text-gray-800">Kategori Produk</h1>
-    <p class="mb-4">Kategori produk untuk toko Anda dapat diatur di sini. Untuk mengganti urutan kategori di ujung depan, Anda dapat seret-lepas untuk mengurutkannya. Untuk melihat kategori lainnya klik tautan "Opsi Layar" pada bagian atas halaman.</p>
-
-    <!-- Content Row -->
-    <div class="row">
-
-        <!-- Left Panel -->
-        <div class="col-lg-6 mb-2">
-
-            <div class="card position-relative">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Kategori dan Sub kategori</h6>
-                </div>
-                <div class="card-body">
-                    <!-- form -->
-                    <form action="/kategori/create">
-
-                        <div class="mb-3">
-                            <label for="kategori" class="form-label">Nama</label>
-                            <input type="text" class="form-control" id="kategori" placeholder="Nama Lengkap" name="kategori">
-                        </div>
-                        <div class="mb-3">
-                            <label for="slug" class="form-label">Slug</label>
-                            <input type="text" class="form-control" id="slug" placeholder="slug" name="slug">
-                        </div>
-                        <div class="mb-1">
-                            <label for="kategori" class="form-label">Induk Kategori</label>
-                        </div>
-                        <div>
-                            <select class="form-select mb-2" aria-label="Default select example">
-                                <option selected>Pilihan</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                        <p class="mb-0 small">Note: Tetapkan sebuah istilah induk untuk membuat sebuah hirarki. Istilah Jazz, contohnya, akan menjadi induk dari Bebop dan Big Band.</p>
-                        <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Deskripsi</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
-                        <div class="small mb-1">Navbar Dropdown Example:</div>
-                        <p class="mb-0 small">Note: Umumnya deskripsi tidak tampil. Namun, beberapa tema dapat menampilkannya.</p>
-                        <div>
-                            <button type="submit" class="btn btn-primary mt-3" id="ka">Simpan</button>
-                        </div>
-                </div>
+        <div class="card position-relative">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">List Produk Kategori</h6>
             </div>
-            </form>
-
-        </div>
-
-        <!-- Right panel -->
-        <div class="col-lg-6">
-
-            <div class="card position-relative">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Produk list</h6>
-                </div>
-                <div class="card-body">
-                    <!-- secarhbar -->
-                    <div class="container text-right">
-                        <div class="row row-cols-2">
-                            <div class="col"></div>
-                            <div class="col">
-                                <!-- searchbar -->
-                                <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
+            <div class="card-body">
+                <!-- code -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-3">
+                    <form class="form-inline ml-auto">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
                             </div>
                         </div>
-                    </div>
-                    <div class="table-responsive mt-3">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Gambar</th>
-                                    <th scope="col">Nama</th>
-                                    <th scope="col">Slug</th>
-                                    <th scope="col">Ketegori</th>
-                                    <th scope="col">Sub-kategori</th>
-                                    <th scope="col">Deskripsi</th>
-                                    <th scope="col">Aksi</th>
+                    </form>
+                </nav>
+                <table class="table table-bordered text-center table-responsive" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>gambar</th>
+                            <th>Nama</th>
+                            <th>Slug</th>
+                            <th>kategori</th>
+                            <th>Subkategori</th>
+                            <th>Deskripsi</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td><img src="<?= base_url() ?>assets/img/produk/p7.png" class="img-fluid" alt="" width="50" height="50"></td>
+                            <td>Nori</td>
+                            <td>-</td>
+                            <td>Makananan Ringan</td>
+                            <td>Makanan Korea</td>
+                            <td>Lezat Begizi</td>
+                            <td> <a href="#" class="btn btn-warning btn-circle btn-sm">
+                                    <i class="fas fa-exclamation-triangle"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-circle btn-sm">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-end">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
 
-
-                                </tr>
-                            </thead>
-                            <tfoot>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td><img src="<?= base_url() ?>assets/img/bg3.jpg" alt="" width="50" height="50"></td>
-                                        <td>contoh</td>
-                                        <td>contoh</td>
-                                        <td>contoh</td>
-                                        <td>contoh</td>
-                                        <td>contoh</td>
-                                        <td>
-                                            <a href="#" class="btn btn-warning btn-circle btn-sm">
-                                                <i class="fas fa-exclamation-triangle"></i>
-                                            </a>
-                                            <a href="#" class="btn btn-danger btn-circle btn-sm">
-                                                <i class="fas fa-trash"></i>
-                                            </a>
-                                        </td>
-                                </tbody>
-                            </tfoot>
-                        </table>
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination justify-content-end">
-                                <li class="page-item">
-                                    <a class="page-link" href="#" aria-label="Previous">
-                                        <span aria-hidden="true">&laquo;</span>
-                                    </a>
-                                </li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#" aria-label="Next">
-                                        <span aria-hidden="true">&raquo;</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
             </div>
-
         </div>
 
     </div>
 
 </div>
-<!-- /.container-fluid -->
-
-
-
-
 
 
 <?= $this->endSection(); ?>
