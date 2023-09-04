27d6262f08b61cb1bea62b25a86c17d7f4183360
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-09-04 14:06:19 +0700

    Update Detail Pesanan
    - Fetching Data Alamat User
    - Fetching Data Detail Pesanan pada Timeline Pesanan
    - Sedikit perbaikan pada view Detail Pesanan

--- app/Controllers/Status.php
+++ app/Controllers/Status.php
@@ -2,23 +2,25 @@
 
 namespace App\Controllers;
 
+use App\Models\CheckoutModel;
+use App\Models\AlamatUserModel;
+use App\Models\StatusPesanModel;
+
 class Status extends BaseController
 {
     public function status(): string
     {
+        $checkoutModel = new CheckoutModel();
+        $alamatModel = new AlamatUserModel();
+        $statusModel = new StatusPesanModel();
+
+        $alamat_list = $alamatModel->where('id_user', user_id())->findAll();
+        $status_list = $statusModel->findAll();
+
         $data = [
             'title'                     => 'Status Pesanan',
-            'nama'                      => 'Javasuperfood',
-            'telp'                      => '+62 123456789',
-            'label'                     => 'Kantor',
-            'alamat'                    => 'Ruko Cyber Park Jalan Gajah Mada Jalan Boulevard Jendral Sudirman No.2159/2161/2165, RT.001/RW.009, Panunggangan Bar., Kec. Cibodas, Kota Tangerang, Banten 15139',
-            'catatan'                   => 'Rumah saya ada anjing nya. Awas di gigit. Anjing saya rabies.',
-            'keterangan'                => [
-                'diterima'              => ['pesan' => 'Paket Diterima', 'tanggal' => '21 Agustus 2023'],
-                'diproses'              => ['pesan' => 'Paket Diproses', 'tanggal' => '23 Agustus 2023'],
-                'dikirim'               => ['pesan' => 'Paket Dikirim', 'tanggal'  => '24 Agustus 2023'],
-                'terkirim'              => ['pesan' => 'Paket Terkirim', 'tanggal' => '25 Agustus 2023'],
-            ],
+            'getalamat'                 => $alamat_list,
+            'getstatus'                 => $status_list,
         ];
         return view('user/produk/status', $data);
     }
