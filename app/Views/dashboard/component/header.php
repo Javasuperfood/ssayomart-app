0caaa0765f332dcb6bdad00b0e00daacea92b9d0
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-09-06 13:20:31 +0700

    update

--- app/Views/dashboard/component/header.php
+++ app/Views/dashboard/component/header.php
@@ -8,14 +8,14 @@
 <title>Dashboard Super Admin</title>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<link href="<?= base_url(); ?>assets/sb-admin2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="<?= base_url(); ?>assets/sb-admin2/vendor-sb/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 <!-- Custom styles for this template -->
 <link href="<?= base_url(); ?>assets/sb-admin2/css/sb-admin-2.min.css" rel="stylesheet">
 
 <!-- Custom styles for this page -->
-<link href="<?= base_url(); ?>assets/sb-admin2/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<link href="<?= base_url(); ?>assets/sb-admin2/vendor-sb/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 <script src="<?= base_url(); ?>assets/sweetalert2/sweetalert2.all.min.js"></script>
 
