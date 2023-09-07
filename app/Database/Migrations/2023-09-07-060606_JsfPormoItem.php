b32af3e8374eacbbdf1a6f095310cf0bdb454147
kikukeii <miftakhuddinfalaki@gamail.com>
2023-09-07 13:16:31 +0700

    create migration promo and promo item

---
+++ app/Database/Migrations/2023-09-07-060606_JsfPormoItem.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+use function PHPSTORM_META\type;
+
+class JsfPormoItem extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_promo_item' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_promo' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'id_produk' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+            ],
+            'discount' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+                'default' => 0.00,
+            ],
+            'min' => [
+                'type' => 'int',
+                'constraint'     => 11,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type'          => 'timestamp',
+                'null'          => true
+            ], 'updated_at' => [
+                'type'          => 'timestamp',
+                'null'          => true
+            ],
+        ]);
+        $this->forge->addKey('id_promo_produk', true);
+        $this->forge->addForeignKey('id_promo', 'jsf_promo', 'id_promo');
+        $this->forge->createTable('jsf_promo_produk');
+    }
+
+    public function down()
+    {
+        $this->forge->dropForeignKey('jsf_promo', 'id_promo');
+
+        $this->forge->dropTable('jsf_promo_produk');
+    }
+}
