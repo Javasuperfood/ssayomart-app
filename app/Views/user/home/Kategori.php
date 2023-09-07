96bad9fec83d2f6ea9584498cad8f33983157b19
rizqiramadhani1101 <rizqiramadhani37@gmail.com>
2023-09-07 16:09:48 +0700

    delete asset

--- app/Views/user/home/Kategori.php
+++ app/Views/user/home/Kategori.php
@@ -1,8 +1,6 @@
 <?= $this->extend('user/home/layout') ?>
 <?= $this->section('page-content') ?>
-
 <?= $this->include('user/home/component/navbarTop') ?>
-
 <?= $this->include('user/home/component/slider') ?>
 
 <!-- ITEM -->
