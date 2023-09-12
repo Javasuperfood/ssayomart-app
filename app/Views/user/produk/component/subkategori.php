8633b95b3d2b89d573f0e7e3bbc04eebf7e6034c
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-09-12 16:14:12 +0700

    Update subkategori.php

--- app/Views/user/produk/component/subkategori.php
+++ app/Views/user/produk/component/subkategori.php
@@ -6,7 +6,7 @@
                     <div class="swiper-wrapper">
                         <?php foreach ($subKategori as $s) : ?>
                             <div class="swiper-slide mx-3">
-                                <a href="<?= base_url('produk/kategori/' . $s['slugS']); ?>" class="btn border-0 btn-custom-rounded text-danger" style="width: 200px;">
+                                <a href="<?= base_url(); ?>produk/kategori/<?= $s['slugK']; ?>/<?= $s['slugS']; ?>" class="btn border-0 btn-custom-rounded text-danger" style="width: 200px;">
                                     <?= $s['nama_kategori']; ?>
                                 </a>
                             </div>
