bda7e188f18cabad9dabb7591aeb6d8b4a9ea886
kikukeii <miftakhuddinfalaki@gamail.com>
2023-09-14 08:50:46 +0700

    updete seeder produk

--- app/Database/Seeds/Produk.php
+++ app/Database/Seeds/Produk.php
@@ -8,37 +8,82 @@ class Produk extends Seeder
 {
     public function run()
     {
-        $lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum incidunt velit, in architecto maiores corporis explicabo quos odio facere dolores minima est, fugit doloribus similique, numquam alias consequatur neque ducimus?';
-        $data = [
-            [
-                'nama' => 'NoriGo',
-                'slug'    => 'nori-1',
-                'sku'    => 1,
-                'harga'    => 20000,
-                'deskripsi'    => $lorem,
-                'id_kategori' => 1,
-                'id_sub_kategori' => 1
-            ],
-            [
-                'nama' => 'NoriGo 2',
-                'slug'    => 'nori-2',
-                'sku'    => 1,
-                'harga'    => 20000,
-                'deskripsi'    => $lorem,
-                'id_kategori' => 1,
-                'id_sub_kategori' => 2
-            ],
-            [
-                'nama' => 'Daging Sapi',
-                'slug'    => 'daging-sapi',
-                'sku'    => 1,
-                'harga'    => 20000,
-                'deskripsi'    => $lorem,
-                'id_kategori' => 2,
-                'id_sub_kategori' => 3
-            ],
-        ];
+        // $lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum incidunt velit, in architecto maiores corporis explicabo quos odio facere dolores minima est, fugit doloribus similique, numquam alias consequatur neque ducimus?';
+        // $data = [
+        //     [
+        //         'nama' => 'NoriGo',
+        //         'slug'    => 'nori-1',
+        //         'sku'    => 1,
+        //         'harga'    => 20000,
+        //         'deskripsi'    => $lorem,
+        //         'id_kategori' => 1,
+        //         'id_sub_kategori' => 1
+        //     ],
+        //     [
+        //         'nama' => 'NoriGo 2',
+        //         'slug'    => 'nori-2',
+        //         'sku'    => 1,
+        //         'harga'    => 20000,
+        //         'deskripsi'    => $lorem,
+        //         'id_kategori' => 1,
+        //         'id_sub_kategori' => 2
+        //     ],
+        //     [
+        //         'nama' => 'Daging Sapi',
+        //         'slug'    => 'daging-sapi',
+        //         'sku'    => 1,
+        //         'harga'    => 20000,
+        //         'deskripsi'    => $lorem,
+        //         'id_kategori' => 2,
+        //         'id_sub_kategori' => 3
+        //     ],
+        // ];
 
-        $this->db->table('jsf_produk')->insertBatch($data);
+        // $this->db->table('jsf_produk')->insertBatch($data);
+
+        $harga = [
+            '1' => 10000,
+            '2' => 15000,
+            '3' => 20000,
+            '4' => 25000,
+            '5' => 30000,
+            '6' => 40000,
+            '7' => 50000,
+            '8' => 100000,
+        ];
+        for ($i = 1; $i <= 2000; $i++) {
+            $idK = mt_rand(1, 17);
+            $idS = null;
+            if ($idK == 2) {
+                $idS = mt_rand(1, 3);
+            }
+            if ($idK == 3) {
+                $idS = mt_rand(4, 5);
+            }
+            if ($idK == 4) {
+                $idS = mt_rand(6, 7);
+            }
+            if ($idK == 5) {
+                $idS = mt_rand(8, 10);
+            }
+            if ($idK == 6) {
+                $idS = mt_rand(11, 14);
+            }
+            if ($idK == 7) {
+                $idS = mt_rand(15, 18);
+            }
+            $faker = \Faker\Factory::create();
+            $nama = $faker->sentence(mt_rand(1, 2));
+            $data = [
+                'nama' => $nama,
+                'slug'    => url_title($nama, '-', true),
+                'sku'    => $faker->numberBetween(1000000, 9000000),
+                'harga'    => $harga[mt_rand(1, 8)],
+                'deskripsi'    => $faker->paragraph(2),
+                'id_kategori' => $idK,
+                'id_sub_kategori' => $idS
+            ];
+            $this->db->table('jsf_produk')->insert($data);
+        }
     }
 }
