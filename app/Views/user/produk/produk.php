52be47c836d84cf2a89747ae2a521518a7a78be2
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-09-20 09:25:56 +0700

    Update produk.php

--- app/Views/user/produk/produk.php
+++ app/Views/user/produk/produk.php
@@ -1,101 +1,148 @@
 <?= $this->extend('user/home/layout2') ?>
 <?= $this->section('page-content') ?>
 
+<?php
+// Mendeteksi User-Agent
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+// Menentukan apakah pengguna menggunakan perangkat seluler (misalnya, smartphone atau tablet)
+$isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Tablet') !== false);
+?>
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6">
-            <img src="<?= base_url() ?>assets/img/produk/main/<?= $produk['img']; ?>" class="img-fluid" alt="<?= $produk['nama']; ?>">
-        </div>
-        <!-- View Mobile -->
-        <div class="col-md-6 mt-4 d-md-none">
-            <h2><?= $produk['nama']; ?></h2>
+<?php if ($isMobile) : ?>
+    <div id="mobileContent">
+        <div class="container mt-5">
             <div class="row">
-                <div class="col">
-                    <h1>Rp. <?= number_format($produk['harga'], 0, ',', '.'); ?></h1>
+                <div class="col-md-6">
+                    <img src="<?= base_url() ?>assets/img/produk/main/<?= $produk['img']; ?>" class="img-fluid" alt="<?= $produk['nama']; ?>">
                 </div>
-                <div class="col text-end">
-                    <a role="button" type="submit" class="add-to-wishlist-btn fw-bold link-underline link-underline-opacity-0 link-dark" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>><i class=" fa-regular fa-heart"></i> Add to Wishlist</a>
+                <!-- View Mobile -->
+                <div class="col-md-6 mt-4 d-md-none">
+                    <h2><?= $produk['nama']; ?></h2>
+                    <div class="row">
+                        <div class="col">
+                            <h1>Rp. <?= number_format($produk['harga'], 0, ',', '.'); ?></h1>
+                        </div>
+                        <div class="col text-end">
+                            <a role="button" type="submit" class="add-to-wishlist-btn fw-bold link-underline link-underline-opacity-0 link-dark" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>"><i class="fa-regular fa-heart"></i> Add to Wishlist</a>
+                        </div>
+                    </div>
+                    <div class="container pt-3">
+                        <div class="row px-5">
+                            <div class="col px-5">
+                                <div class="input-group mb-3 d-flex justify-content-center">
+                                    <button class="btn btn-outline-danger rounded-circle" type="button" onClick='decreaseCount(event, this)'><i class="bi bi-dash"></i></button>
+                                    <input type="number" class="form-control text-center bg-white border-0" disabled value="1">
+                                    <button class=" btn btn-outline-danger rounded-circle" type="button" onClick='increaseCount(event, this)'><i class="bi bi-plus"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <button class="btn btn-white text-danger border-danger mt-4 add-to-cart-btn d-inline" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>"><i class=" bi bi-basket2"></i></button>
+                        <form action="<?= base_url('buy/' . $produk['slug']); ?>" method="get" class="d-inline">
+                            <input type="hidden" id="qty" name="qty" value="1">
+                            <button type="submit" class="btn btn-white text-danger border-danger mt-4">Beli Sekarang</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="row mt-4 mb-5">
+                    <div class="col-lg-6">
+                        <h2 class="text-merah"> Deskripsi </h2>
+                        <p class="text-potong"><?= $produk['deskripsi']; ?></p>
+                        <!-- <button class="btn btn-danger mb-5" onclick="myFunction()" id="myBtn">Read more</button> -->
+                    </div>
                 </div>
             </div>
-            <div class="container pt-3">
-                <div class="row px-5">
-                    <div class="col px-5">
-                        <div class="input-group mb-3 d-flex justify-content-center">
-                            <button class="btn btn-outline-danger rounded-circle" type="button" onClick='decreaseCount(event, this)'><i class="bi bi-dash"></i></button>
-                            <input type="number" class="form-control text-center bg-white border-0" disabled value="1">
-                            <button class=" btn btn-outline-danger rounded-circle" type="button" onClick='increaseCount(event, this)'><i class="bi bi-plus"></i></button>
+            <div class="row pt-5">
+                <div class="col"></div>
+            </div>
+        </div>
+    </div>
+<?php else : ?>
+    <!-- Akhir view mobile -->
+
+    <!-- View Desktop -->
+    <div id="desktopContent" style="margin-top:100px;">
+        <div class="container d-none d-md-block">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?= base_url() ?>assets/img/produk/main/<?= $produk['img']; ?>" class="img-fluid" alt="<?= $produk['nama']; ?>">
+                </div>
+                <div class="col-md-6">
+                    <h2><?= $produk['nama']; ?></h2>
+                    <div class="row">
+                        <div class="col">
+                            <h1>Rp. <?= number_format($produk['harga'], 2, ',', '.'); ?></h1>
+                        </div>
+                        <div class="text mt-2">
+                            <a role="button" type="submit" class="add-to-wishlist-btn fw-bold link-underline link-underline-opacity-0 link-dark" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>">
+                                <i class="bi bi-heart"></i> Add to Wishlist
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row-5 mt-4">
+                        <div class="col-4">
+                            <div class="input-group">
+                                <button class="btn btn-outline-danger" type="button" onClick="decreaseCount(event, this)">
+                                    <i class="bi bi-dash"></i>
+                                </button>
+                                <input type="number" class="form-control text-center bg-white border-0" disabled value="1">
+                                <button class="btn btn-outline-danger mr-4" type="button" onClick="increaseCount(event, this)">
+                                    <i class="bi bi-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn btn-white text-danger border-danger mt-4 add-to-cart-btn d-inline" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>"><i class=" bi bi-basket2"></i></button>
+                        <form action="<?= base_url('buy/' . $produk['slug']); ?>" method="get" class="d-inline">
+                            <input type="hidden" id="qty" name="qty" value="1">
+                            <button type="submit" class="btn btn-white text-danger border-danger mt-4">Beli Sekarang</button>
+                        </form>
+                    </div>
+                    <div class="row my-4">
+                        <div class="col-lg-6">
+                            <h2 class="text-merah"> Deskripsi </h2>
+                            <p class="text-potong"><?= $produk['deskripsi']; ?></p>
+                            <!-- <button class="btn btn-danger mb-5" onclick="myFunction()" id="myBtn">Read more</button> -->
                         </div>
                     </div>
                 </div>
             </div>
-            <br>
-            <div class="text-center">
-                <button class="btn btn-white text-danger border-danger mt-4 add-to-cart-btn d-inline" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>"><i class=" bi bi-basket2"></i></button>
-                <form action="<?= base_url('buy/' . $produk['slug']); ?>" method="get" class="d-inline">
-                    <input type="hidden" id="qty" name="qty" value="1">
-                    <button type="submit" class="btn btn-white text-danger border-danger mt-4">Beli Sekarang</button>
-                </form>
-            </div>
         </div>
-        <!-- Akhir view mobile -->
-        <!-- View Desktop -->
-        <div class="col-md-6 mt-4 d-none d-md-block">
-            <h2><?= $produk['nama']; ?></h2>
-            <div class="row">
-                <div class="col">
-                    <h1>Rp. <?= number_format($produk['harga'], 2, ',', '.'); ?></h1>
-                </div>
-                <div class="text mt-2">
-                    <a role="button" type="submit" class="add-to-wishlist-btn fw-bold link-underline link-underline-opacity-0 link-dark" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>">
-                        <i class="bi bi-heart"></i> Add to Wishlist
-                    </a>
-                </div>
-            </div>
-
-            <div class="row-5 mt-4">
-                <div class="col-4">
-                    <div class="input-group">
-                        <button class="btn btn-outline-danger" type="button" onClick="decreaseCount(event, this)">
-                            <i class="bi bi-dash"></i>
-                        </button>
-                        <input type="number" class="form-control text-center bg-white border-0" disabled value="1">
-                        <button class="btn btn-outline-danger mr-4" type="button" onClick="increaseCount(event, this)">
-                            <i class="bi bi-plus"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <br>
-            <div class="text">
-                <input type="hidden" id="qty" name="qty" value="1">
-                <button class="btn btn-white text-danger border-danger mt-4 add-to-cart-btn" produk="<?= $produk['id_produk']; ?>" harga="<?= $produk['harga']; ?>"><i class=" bi bi-basket2"></i></button>
-                <a href="<?= base_url() ?>checkout" class="btn btn-white text-danger border-danger mt-4 ">Beli Sekarang</a>
-            </div>
-        </div>
-        <!-- akhir view desktop -->
-        <div class="row mt-4 mb-5">
-            <div class="col-lg-6">
-                <h2 class="text-merah"> Deskripsi </h2>
-                <p class="text-potong"><?= $produk['deskripsi']; ?></p>
-                <p class="col">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel<span id="dots">...</span><span id="more">erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span></p>
-                <button class="btn btn-danger mb-5" onclick="myFunction()" id="myBtn">Read more</button>
-            </div>
+        <div class="row pt-5">
+            <div class="col"></div>
         </div>
     </div>
+<?php endif; ?>
+<!-- end Desktop -->
+
+<?php
+if ($isMobile) {
+
+    echo '<div id="mobileContent">';
+
+    echo '</div>';
+} else {
+
+    echo '<div id="desktopContent">';
+
+    echo '</div>';
+}
+?>
+<!-- akhir view desktop -->
 
 
-    <!-- Deskripsi read more tampilan web  -->
-    <div class="d-none d-lg-block">
-        <p class="col-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel<span id="dots">...</span><span id="more">erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span></p>
 
-        <button class="btn btn-danger" onclick="myFunction()" id="myBtn">Read more</button>
-        <!-- Deskripsi read more tampilan web  -->
+<!-- Deskripsi read more tampilan web  -->
+<!-- <div class="d-none d-lg-block"> -->
+<!-- <p class="col-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel<span id="dots">...</span><span id="more">erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span></p>
 
+        <button class="btn btn-danger" onclick="myFunction()" id="myBtn">Read more</button> -->
+<!-- Deskripsi read more tampilan web  -->
 
-        <!-- swiper card tampilan web -->
-        <div class="d-none d-lg-block">
+<!-- produk terkait section tampilan web -->
+<!-- <div class="d-none d-lg-block">
             <div class="row mt-3">
                 <h2 class="text-merah"> Product Terkait </h2>
                 <div class="col">
@@ -235,17 +282,16 @@
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </div>
 
-        </div>
-        <!-- swiper card tampilan web-->
+        </div> -->
+<!-- produk terkait section tampilan web-->
 
-        <!-- 6 card tampilan web -->
-        <div class="d-none d-lg-block">
+<!-- produk terbaru section tampilan web -->
+<!-- <div class="d-none d-lg-block">
             <div class="row mt-3">
                 <h2 class="text-merah"> Product Terbaru </h2>
                 <div class="col-6 col-md-4 col-lg-2 mb-3">
@@ -260,22 +306,11 @@
                                 <a href="#" class="btn btn-danger "> <i class="bi bi-basket"></i></a>
                             </p>
                         </div>
-                        <!-- Tombol Navigasi -->
                     </div>
                 </div>
             </div>
-
-        </div>
-        <!-- 6 card tampilan web-->
-
-    </div>
-</div>
-
-
-
-<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
+        </div> -->
+<!-- produk terbaru section tampilan web-->
 
 <script type="text/javascript">
     function increaseCount(a, b) {
@@ -299,11 +334,7 @@
         }
     }
 </script>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-
-<script>
+<!-- <script>
     function myFunction() {
         var dots = document.getElementById("dots");
         var moreText = document.getElementById("more");
@@ -319,7 +350,7 @@
             moreText.style.display = "inline";
         }
     }
-</script>
+</script> -->
 
 <?= $this->include('user/component/scriptAddToCart'); ?>
 <?= $this->include('user/component/scriptAddToWishlist'); ?>
