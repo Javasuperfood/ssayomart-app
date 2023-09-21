10e179ee832ca3f20c0afa56d9210ddea7d3ce62
kikukeii <miftakhuddinfalaki@gamail.com>
2023-09-21 11:03:09 +0700

    update script number input

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,17 @@
 // onkeypress="return isNumber(event);" 
 // pada tag input
 function isNumber(e) {
-    e = e || window.event;
-    var charCode = e.which ? e.which : e.keyCode;
-    return /\d/.test(String.fromCharCode(charCode));
+    var charCode = (e.which) ? e.which : e.keyCode;
+  
+    // Check if the input is a number or control key
+    if (
+      (charCode >= 48 && charCode <= 57) ||  // Numbers 0-9
+      (charCode === 8) ||                   // Backspace
+      (charCode === 37) ||                  // Left Arrow (for navigation)
+      (charCode === 39)                     // Right Arrow (for navigation)
+    ) {
+      return true;
+    }
+  
+    return false;
 }
