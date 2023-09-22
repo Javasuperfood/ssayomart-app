c3f3a3db94fa0291561441dcff35f7e3a29ad54b
Gilang Aditia <gaditia744@gmail.com>
2023-09-22 08:58:14 +0700

    update baner spesial sayo ,tmbh produk,diskon deks

--- app/Views/dashboard/produk/tambahProduk.php
+++ app/Views/dashboard/produk/tambahProduk.php
@@ -8,195 +8,81 @@
     <b>MOHON TELITI KETIKA MENGISI PRODUK UNTUK MENGHINDARI KESALAHAN YANG TIDAK DIINGINKAN!!</b>
 </div>
 
-<div class="row">
-    <!-- Left Panel -->
-    <div class="col-md-6">
-        <div class="card border-0 shadow-sm position-relative">
-            <div class="card-header border-0 py-3">
-                <h6 class="m-0 font-weight-medium">Masukan Produk Baru</h6>
-            </div>
-            <div class="card-body">
-                <!-- code -->
-                <form action="<?= base_url(); ?>dashboard/produk/tambah-produk/save" onsubmit="return validasiTambahProduk()" method="post" enctype="multipart/form-data">
-                    <?= csrf_field(); ?>
-                    <div class="mb-3">
-                        <label for="nama" class="form-label">Nama Produk</label>
-                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Produk Anda..." value="<?= old('nama') ?>">
-                        <span id="produkError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <label for="sku" class="form-label">Stock Keeping Unit (SKU)</label>
-                        <input type="text" class="form-control" id="sku" name="sku" placeholder="SKU Produk Anda..." value="<?= old('sku') ?>" onkeypress="return isNumber(event);">
-                        <span id="skuError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <label for="deskripsi">Deskripsi Produk</label>
-                        <textarea class="form-control" id="deskripsi" name="deskripsi" placeholder="Deskripsi Produk Anda .." value="<?= old('deskripsi') ?>"></textarea>
-                        <span id="deskripsiError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <label for="parent_kategori_id">Kategori Induk</label>
-                        <select class="form-control" id="kategori" name="parent_kategori_id">
-                            <option value="">Pilih Kategori</option>
-                            <?php foreach ($kategori as $km) : ?>
-                                <option value="<?= $km['id_kategori']; ?>"><?= $km['nama_kategori']; ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                        <span id="kategoriError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <label for="parent_kategori_id">Sub Kategori</label>
-                        <select class="form-control" id="sub_kategori" name="sub_kategori">
-                            <option value="">Pilih Kategori</option>
-                        </select>
-                        <span id="kategoriError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <label for="img" class="form-label">Gambar/Foto Produk</label>
-                        <input type="file" class="form-control" id="img" name="img" placeholder="Masukan Gambar Produk">
-                        <span id="imgError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label for="selectVariant">Pilih Variant</label>
-                                <select class="form-control" name="selectVariant" id="selectVariant">
-                                    <option selected>Pilih</option>
-                                    <?php foreach ($variasi as $v) : ?>
-                                        <option value="<?= $v['id_variasi']; ?>"><?= $v['nama_varian']; ?></option>
-                                    <?php endforeach ?>
-                                </select>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="valueVariant">Value Variant</label>
-                                <input type="text" id="valueItem" name="valueItem" class="form-control" placeholder="Value Varian">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="berat" class="form-label">Berat Produk (*Satuan Gram)</label>
-                        <input type="price" class="form-control" id="berat" name="berat" placeholder="Berat Produk Anda..." value="<?= old('berat') ?>" onkeypress="return isNumber(event);">
-                        <span id="beratError" class="text-danger"></span>
-                    </div>
-                    <div class="mb-3">
-                        <label for="harga" class="form-label">Harga Produk</label>
-                        <input type="price" class="form-control" id="harga" name="harga" placeholder="Harga Produk Anda..." value="<?= old('harga') ?>">
-                        <span id="hargaError" class="text-danger"></span>
-                    </div>
-                    <button type="submit" class="btn btn-danger">Simpan</button>
-                </form>
-            </div>
-        </div>
+<div class="card border-0 shadow-sm position-relative">
+    <div class="card-header border-0 py-3">
+        <h6 class="m-0 font-weight-medium">Masukan Produk Baru</h6>
     </div>
-
-    <!-- Right Panel -->
-    <div class="col-md-6 mb-3">
-        <div class="card position-relative border-0 shadow-sm">
-            <div class="card-header border-0 py-3">
-                <h6 class="m-0 font-weight-medium">List Produk</h6>
+    <div class="card-body">
+        <!-- code -->
+        <form action="<?= base_url(); ?>dashboard/produk/tambah-produk/save" onsubmit="return validasiTambahProduk()" method="post" enctype="multipart/form-data">
+            <?= csrf_field(); ?>
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama Produk</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Produk Anda..." value="<?= old('nama') ?>">
+                <span id="produkError" class="text-danger"></span>
             </div>
-            <div class="card-body">
-                <table class="table text-center table-responsive" id="example" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Gambar</th>
-                            <th scope="col">Nama Produk</th>
-                            <!-- <th scope="col">Tanggal EXP</th> -->
-                            <!-- <th scope="col">Ketegori</th> -->
-                            <th scope="col">Harga Produk</th>
-                            <th scope="col">Kategori Produk</th>
-                            <th scope="col">Variant</th>
-                            <!-- <th scope="col">Deskripsi</th> -->
-                            <th style="width: 100px;">Aksi</th>
-                        </tr>
-                    </thead>
-                    <?= $no = 1; ?>
-                    <tbody>
-                        <?php foreach ($produk_Model as $km) : ?>
-                            <tr>
-                                <td><?= $no++; ?></td>
-                                <td>
-                                    <img src="<?= base_url('assets/img/produk/main/' . $km['img']); ?>" class="img-fluid" alt="" width="50" height="50">
-                                </td>
-                                <td><?= $km['nama']; ?></td>
-                                <td><?= $km['harga']; ?></td>
-                                <td style="display: none;">1</td>
-                                <td>
-                                    <?php
-                                    $kategoriNama = '';
-                                    $subKategoriNama = '';
-                                    foreach ($kategori as $k) {
-                                        if ($k['id_kategori'] == $km['id_kategori']) {
-                                            $kategoriNama = $k['nama_kategori'];
-                                            if ($km['id_sub_kategori'] != null) {
-                                                foreach ($subKategori as $s) {
-                                                    if ($s['id_sub_kategori'] == $km['id_sub_kategori']) {
-                                                        $subKategoriNama = $s['nama_kategori'];
-                                                        break;
-                                                    }
-                                                }
-                                            }
-                                            break;
-                                        }
-                                    }
-                                    echo ($kategoriNama !== '') ? $kategoriNama . '<br>(' . $subKategoriNama . ')' : 'Kategori Tidak Ditemukan';
-                                    ?>
-                                </td>
-                                <td>
-
-                                    <?php
-                                    $previousVarian = null;
-                                    foreach ($variasiItem as $vi) : ?>
-                                        <?php
-                                        $i = 0;
-                                        if ($km['id_produk'] == $vi['id_produk']) {
-                                            if ($previousVarian !== $vi['nama_varian']) {
-                                                echo $vi['nama_varian'] . ' : ';
-                                                $previousVarian = $vi['nama_varian'];
-                                            }
-                                            echo '|' . $vi['value_item'] . '|';
-                                        }
-                                        ?>
-                                    <?php endforeach ?>
-                                </td>
-                                <td class="text-center">
-                                    <div class="nav-item dropdown no-arrow">
-                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="bi bi-three-dots-vertical"></i>
-                                        </a>
-                                        <!-- Dropdown - User Information -->
-                                        <div class="dropdown-menu shadow" aria-labelledby="userDropdown">
-                                            <a class="dropdown-item" href="<?= base_url() ?>produk/<?= $km['slug']; ?>">
-                                                <i class="bi bi-eye-fill fa-sm fa-fw mr-2 text-gray-400"></i>
-                                                Lihat Produk
-                                            </a>
-                                            <a class="dropdown-item" href="<?= base_url() ?>dashboard/produk/detail-varian/<?= $km['slug']; ?>">
-                                                <i class="bi bi-eye-fill fa-sm fa-fw mr-2 text-gray-400"></i>
-                                                Detail Varian
-                                            </a>
-                                            <a class="dropdown-item" href="<?= base_url(); ?>dashboard/produk/tambah-produk/update-produk/<?= $km['id_produk']; ?>">
-                                                <i class="bi bi-pen-fill fa-sm fa-fw mr-2 text-gray-400"></i>
-                                                Update
-                                            </a>
-                                            <div class="dropdown-divider"></div>
-                                            <form action="<?= base_url() ?>dashboard/produk/tambah-produk/delete-produk/<?= $km['id_produk']; ?>" method="post">
-                                                <?= csrf_field() ?>
-                                                <button type="submit" class="dropdown-item">
-                                                    <i class="bi bi-trash-fill fa-sm fa-fw mr-2 text-danger"></i>
-                                                    <span class="text-danger">Delete</span>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                    </tbody>
-                <?php endforeach; ?>
-                </table>
+            <div class="mb-3">
+                <label for="sku" class="form-label">Stock Keeping Unit (SKU)</label>
+                <input type="text" class="form-control" id="sku" name="sku" placeholder="SKU Produk Anda..." value="<?= old('sku') ?>" onkeypress="return isNumber(event);">
+                <span id="skuError" class="text-danger"></span>
             </div>
-        </div>
+            <div class="mb-3">
+                <label for="deskripsi">Deskripsi Produk</label>
+                <textarea class="form-control" id="deskripsi" name="deskripsi" placeholder="Deskripsi Produk Anda .." value="<?= old('deskripsi') ?>"></textarea>
+                <span id="deskripsiError" class="text-danger"></span>
+            </div>
+            <div class="mb-3">
+                <label for="parent_kategori_id">Kategori Induk</label>
+                <select class="form-control" id="kategori" name="parent_kategori_id">
+                    <option value="">Pilih Kategori</option>
+                    <?php foreach ($kategori as $km) : ?>
+                        <option value="<?= $km['id_kategori']; ?>"><?= $km['nama_kategori']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <span id="kategoriError" class="text-danger"></span>
+            </div>
+            <div class="mb-3">
+                <label for="parent_kategori_id">Sub Kategori</label>
+                <select class="form-control" id="sub_kategori" name="sub_kategori">
+                    <option value="">Pilih Kategori</option>
+                </select>
+                <span id="kategoriError" class="text-danger"></span>
+            </div>
+            <div class="mb-3">
+                <label for="img" class="form-label">Gambar/Foto Produk</label>
+                <input type="file" class="form-control" id="img" name="img" placeholder="Masukan Gambar Produk">
+                <span id="imgError" class="text-danger"></span>
+            </div>
+            <div class="mb-3">
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="selectVariant">Pilih Variant</label>
+                        <select class="form-control" name="selectVariant" id="selectVariant">
+                            <option selected>Pilih</option>
+                            <?php foreach ($variasi as $v) : ?>
+                                <option value="<?= $v['id_variasi']; ?>"><?= $v['nama_varian']; ?></option>
+                            <?php endforeach ?>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="valueVariant">Value Variant</label>
+                        <input type="text" id="valueItem" name="valueItem" class="form-control" placeholder="Value Varian">
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="berat" class="form-label">Berat Produk (*Satuan Gram)</label>
+                <input type="price" class="form-control" id="berat" name="berat" placeholder="Berat Produk Anda..." value="<?= old('berat') ?>" onkeypress="return isNumber(event);">
+                <span id="beratError" class="text-danger"></span>
+            </div>
+            <div class="mb-3">
+                <label for="harga" class="form-label">Harga Produk</label>
+                <input type="price" class="form-control" id="harga" name="harga" placeholder="Harga Produk Anda..." value="<?= old('harga') ?>">
+                <span id="hargaError" class="text-danger"></span>
+            </div>
+            <button type="submit" class="btn btn-danger">Simpan</button>
+        </form>
     </div>
 </div>
 
