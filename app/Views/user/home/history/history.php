d90908289e326e7aed11f9efb77a2756fc3658d2
kikukeii <miftakhuddinfalaki@gamail.com>
2023-09-27 16:26:25 +0700

    update

--- app/Views/user/home/history/history.php
+++ app/Views/user/home/history/history.php
@@ -62,31 +62,25 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                         $("#colsId" + <?= $t->id_checkout; ?>).attr('data-bs-toggle', 'collapse');
                         $("#colsId" + <?= $t->id_checkout; ?>).attr('href', '#history' + <?= $t->id_checkout; ?>);
                     </script>
-                    <?php
-                    $i = 0;
-                    foreach ($transaksi as $c) : ?>
-                        <?php if ($t->id_checkout == $c->id_checkout) : ?>
-                            <?php
-                            $i++;
-                            if ($i > 1) : ?>
-                                <div class="row pt-3 collapse" id="history<?= $t->id_checkout; ?>">
-                                    <div class="col">
-                                        <div class="card border-0 shadow-sm">
-                                            <div class="card-body">
-                                                <div class="row">
-                                                    <div class="col-3">
-                                                        <img src="<?= base_url(); ?>assets/img/produk/main/<?= $c->img; ?>" alt="Foto Produk" class="card-img">
-                                                    </div>
-                                                    <div class="col-5 position-absolute top-50 start-50 translate-middle">
-                                                        <h5 class="card-title fs-6"><?= substr($c->nama, 0, 10); ?>...</h5>
-                                                        <p class="text-secondary fs-6">Rp. <?= number_format($c->harga, 0, ',', '.'); ?></p>
-                                                    </div>
+                    <?php foreach ($transaksi as $c) : ?>
+                        <?php if ($t->id_checkout_produk == $c->id_checkout_produk) : ?>
+                            <div class="row pt-3 collapse" id="history<?= $t->id_checkout; ?>">
+                                <div class="col">
+                                    <div class="card border-0 shadow-sm">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-3">
+                                                    <img src="<?= base_url(); ?>assets/img/produk/main/<?= $c->img; ?>" alt="Foto Produk" class="card-img">
+                                                </div>
+                                                <div class="col-5 position-absolute top-50 start-50 translate-middle">
+                                                    <h5 class="card-title fs-6"><?= substr($c->nama, 0, 10); ?>...</h5>
+                                                    <p class="text-secondary fs-6">Rp. <?= number_format($c->harga, 0, ',', '.'); ?></p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            <?php endif ?>
+                            </div>
                         <?php endif ?>
                     <?php endforeach ?>
                 <?php endif ?>
@@ -162,54 +156,48 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                         $("#colsId" + <?= $t->id_checkout; ?>).attr('data-bs-toggle', 'collapse');
                         $("#colsId" + <?= $t->id_checkout; ?>).attr('href', '#history' + <?= $t->id_checkout; ?>);
                     </script>
-                    <?php
-                    $i = 0;
-                    foreach ($transaksi as $c) : ?>
-                        <?php if ($t->id_checkout == $c->id_checkout) : ?>
-                            <?php
-                            $i++;
-                            if ($i > 1) : ?>
-                                <!-- perbaiki colspe -->
-                                <div class="collapse" id="history<?= $t->id_checkout; ?>">
-                                    <div class="row">
-                                        <div class="col mx-auto">
-                                            <div class="card border-0 shadow-sm mb-4">
-                                                <div class="card-body">
-                                                    <div class="row">
-                                                        <div class="col-2">
-                                                            <img src="<?= base_url(); ?>assets/img/produk/main/<?= $t->img; ?>" alt="Foto Produk" class="card-img" style="width: 50px;">
-                                                            <div class="position-absolute bottom-0 start-50 translate-middle-x">
-                                                                <a class="link-secondary" href="#" role="button" id="colsId<?= $t->id_checkout; ?>" style="display: none;">
-                                                                    <i class="bi bi-chevron-bar-down fs-4" style="font-weight: bold;"></i>
-                                                                </a>
-                                                            </div>
+                    <?php foreach ($transaksi as $c) : ?>
+                        <?php if ($t->id_checkout_produk == $c->id_checkout_produk) : ?>
+                            <!-- perbaiki colspe -->
+                            <div class="collapse" id="history<?= $t->id_checkout; ?>">
+                                <div class="row">
+                                    <div class="col mx-auto">
+                                        <div class="card border-0 shadow-sm mb-4">
+                                            <div class="card-body">
+                                                <div class="row">
+                                                    <div class="col-2">
+                                                        <img src="<?= base_url(); ?>assets/img/produk/main/<?= $t->img; ?>" alt="Foto Produk" class="card-img" style="width: 50px;">
+                                                        <div class="position-absolute bottom-0 start-50 translate-middle-x">
+                                                            <a class="link-secondary" href="#" role="button" id="colsId<?= $t->id_checkout; ?>" style="display: none;">
+                                                                <i class="bi bi-chevron-bar-down fs-4" style="font-weight: bold;"></i>
+                                                            </a>
                                                         </div>
-                                                        <div class="col-10 position-absolute top-50 start-50 translate-middle">
-                                                            <h5 class="card-title fs-6"><?= substr($t->nama, 0, 10); ?>...</h5>
-                                                            <p class="text-secondary fs-6">Rp. <?= number_format($t->harga, 0, ',', '.'); ?></p>
-                                                        </div>
-                                                        <?php if ($t->id_status_pesan == 1) : ?>
-                                                            <div class="col-2 position-absolute top-50 end-0 translate-middle-y">
-                                                                <a href="<?= base_url('checkout/' . $t->id_checkout); ?>" class="btn btn-outline-danger">Transaksi Tertunda</a>
-                                                            </div>
-                                                        <?php endif ?>
-                                                        <?php if ($t->id_status_pesan == 2) : ?>
-                                                            <div class="col-2 position-absolute top-50 end-0 translate-middle-y">
-                                                                <a href="<?= base_url('status/' . $t->invoice); ?>" class="btn btn-outline-warning">Dalam Proses</a>
-                                                            </div>
-                                                        <?php endif ?>
-                                                        <?php if ($t->id_status_pesan == 3) : ?>
-                                                            <div class="col-2 position-absolute top-50 end-0 translate-middle-y">
-                                                                <a href="#" class="btn btn-outline-success">Beli Lagi</a>
-                                                            </div>
-                                                        <?php endif ?>
                                                     </div>
+                                                    <div class="col-10 position-absolute top-50 start-50 translate-middle">
+                                                        <h5 class="card-title fs-6"><?= substr($t->nama, 0, 10); ?>...</h5>
+                                                        <p class="text-secondary fs-6">Rp. <?= number_format($t->harga, 0, ',', '.'); ?></p>
+                                                    </div>
+                                                    <?php if ($t->id_status_pesan == 1) : ?>
+                                                        <div class="col-2 position-absolute top-50 end-0 translate-middle-y">
+                                                            <a href="<?= base_url('checkout/' . $t->id_checkout); ?>" class="btn btn-outline-danger">Transaksi Tertunda</a>
+                                                        </div>
+                                                    <?php endif ?>
+                                                    <?php if ($t->id_status_pesan == 2) : ?>
+                                                        <div class="col-2 position-absolute top-50 end-0 translate-middle-y">
+                                                            <a href="<?= base_url('status/' . $t->invoice); ?>" class="btn btn-outline-warning">Dalam Proses</a>
+                                                        </div>
+                                                    <?php endif ?>
+                                                    <?php if ($t->id_status_pesan == 3) : ?>
+                                                        <div class="col-2 position-absolute top-50 end-0 translate-middle-y">
+                                                            <a href="#" class="btn btn-outline-success">Beli Lagi</a>
+                                                        </div>
+                                                    <?php endif ?>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            <?php endif ?>
+                            </div>
                         <?php endif ?>
                     <?php endforeach ?>
                 <?php endif ?>
