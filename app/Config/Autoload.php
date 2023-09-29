d5dd8421d53caee53c7c064af3b0afa1a4c0ee35
kikukeii <miftakhuddinfalaki@gamail.com>
2023-09-29 09:20:02 +0700

    update autoloader

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -47,7 +47,7 @@ class Autoload extends AutoloadConfig
     public $psr4 = [
         APP_NAMESPACE => APPPATH, // For custom app namespace
         'Config'      => APPPATH . 'Config',
-        'Pager' => APPPATH . 'Config/pager',
+        'Pager' => APPPATH . 'App\Views\Pager\pager',
     ];
 
     /**
