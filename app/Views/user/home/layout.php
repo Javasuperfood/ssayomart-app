bf4976e768fb7aafbf1618b0b03e54cf3b5cb57e
rizqiramadhani1101 <rizqiramadhani37@gmail.com>
2023-10-17 15:11:47 +0700

    edit navbar hover button done

--- app/Views/user/home/layout.php
+++ app/Views/user/home/layout.php
@@ -16,7 +16,7 @@
 <body>
     <?= $this->include('user/home/component/preloader') ?>
     <?= $this->renderSection('page-content') ?>
-    <div class="pb-4"></div>
+    <div class="pb-5"></div>
     <?= $this->include('user/home/component/navbarBottom') ?>
     <!-- Bootstrap JS -->
     <script src="<?= base_url() ?>assets/js/jquery/jquery.min.js"></script>
