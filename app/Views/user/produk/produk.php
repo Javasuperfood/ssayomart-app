4b3011c205a2d9da5c909778e25a3004a0152d75
kikuKeii <miftakhuddinfalaki@gmail.com>
2023-10-27 07:56:07 +0700

    update random produk

--- app/Views/user/produk/produk.php
+++ app/Views/user/produk/produk.php
@@ -70,73 +70,7 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                 </div>
             </div>
 
-            <div class="row mt-3">
-                <div class="col">
-                    <h2 class="mb-4 text-merah">Produk Lainnya</h2>
-                    <div class="d-flex justify-content-center align-items-center swiper mySwiper">
-                        <div class="swiper-wrapper d-flex mb-3">
-                            <?php foreach ($randomProducts as $p) : ?>
-                                <div class="swiper-slide col-md-4 mx-md-1 mb-md-1">
-                                    <div class="card border-0 shadow-sm" style="width: auto; height: 100%;">
-                                        <a href="<?= base_url() ?>produk/<?= $p['slug']; ?>" class="link-underline link-underline-opacity-0">
-                                            <div class="d-flex justify-content-center align-items-center">
-                                                <img src="<?= base_url() ?>assets/img/produk/main/<?= $p['img']; ?>" class="card-img-top mt-3 text-center py-0 px-0 mx-0 my-0" alt="..." style="width: 200px; height: 200px;">
-                                            </div>
-                                        </a>
-                                        <div class="fs-2 mt-2" style="padding: 0 10px 0 10px;">
-                                            <div class="d-flex align-items-start justify-content-center" style="height: 65px;">
-                                                <p class=" text-secondary fw-bold " style=" font-size: 13px; margin: 0;"><?= substr($p['nama'], 0, 40); ?>...</p>
-                                            </div>
-                                            <p class="text-secondary text-center" style="font-size: 12px; margin: 0;">
-                                                <del>Rp. <?= number_format($p['harga_min'], 0, ',', '.'); ?></del>
-                                            </p>
-
-                                            <h1 class="text-danger fs-bold mt-1 text-center" style="font-size: 18px; margin: 0;">
-                                                <?php if ($p['harga_min'] == $p['harga_max']) : ?>
-                                                    Rp. <?= number_format($p['harga_min'], 0, ',', '.'); ?>
-                                                <?php else : ?>
-                                                    <?= substr('Rp. ' . number_format($p['harga_min'], 0, ',', '.') . '-' . number_format($p['harga_max'], 0, ',', '.'), 0, 13); ?>...
-                                                <?php endif ?>
-                                            </h1>
-
-                                            <div class="container mt-2">
-                                                <div class="row justify-items-center">
-                                                    <div class="col">
-                                                        <div class="horizontal-counter">
-                                                            <button class="btn btn-outline-danger rounded-circle" type="button" onClick='decreaseCount(event, this)'><i class="bi bi-dash"></i></button>
-                                                            <input type="number" id="counterProduct" class="form-control text-center bg-white border-0" disabled value="1">
-                                                            <button class=" btn btn-outline-danger rounded-circle" type="button" onClick='increaseCount(event, this)'><i class="bi bi-plus"></i></button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="text-center custom-button pb-3" style="display: flex; justify-content: center;">
-                                                <form action="<?= base_url('produk/' . $p['slug']); ?>">
-                                                    <input type="hidden" name="add-to-cart" value="show">
-                                                    <input type="hidden" name="qty" id="Cqty<?= $p['id_produk']; ?>" value="1" value="show">
-                                                    <button type="submit" class="btn btn-danger mx-1 mt-2 fw-bold">
-                                                        <i class="bi bi-basket"></i>
-                                                    </button>
-                                                </form>
-                                                <form action="<?= base_url('produk/' . $p['slug']); ?>">
-                                                    <input type="hidden" name="buy" value="show">
-                                                    <input type="hidden" name="qty" id="Bqty<?= $p['id_produk']; ?>" value="1" value="show">
-                                                    <button type="submit" class="btn btn-danger mx-1 mt-2 fw-bold">
-                                                        Beli
-                                                    </button>
-                                                    <span class="badge text-bg-success position-absolute start-0 top-0" style="font-size: 12px; padding: 2px 4px;">10%</span>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php endforeach; ?>
-                            <?= $this->include('user/component/scriptAddToCart'); ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <?= $this->include('user/produk/component/randomProduk'); ?>
 
             <?php if ($varianItem > 1) : ?>
                 <!-- Modal Varian Buy -->
@@ -284,63 +218,7 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                 </div>
             </div>
             <div class="container d-none d-lg-block">
-                <div class="d-none d-lg-block">
-                    <div class="row mt-3">
-                        <div class="col">
-                            <h2 class="mb-4">Produk Lainnya</h2>
-                            <div class="swiper mySwiper">
-                                <div class="swiper-wrapper d-flex">
-                                    <?php foreach ($randomProducts as $p) : ?>
-                                        <div class="swiper-slide col-md-4 mx-md-1 mb-md-1">
-                                            <div class="card border-0 shadow-sm" style="width: auto; height: 100%;">
-                                                <a href="<?= base_url() ?>produk/<?= $p['slug']; ?>" class="link-underline link-underline-opacity-0">
-                                                    <div class="d-flex justify-content-center align-items-center">
-                                                        <img src="<?= base_url() ?>assets/img/produk/main/<?= $p['img']; ?>" class="card-img-top mt-3 text-center py-0 px-0 mx-0 my-0" alt="..." style="width: 200px; height: 200px;">
-                                                    </div>
-                                                </a>
-                                                <div class="fs-3 mt-3" style="padding: 0 10px 0 10px;">
-                                                    <div class="d-flex align-items-start justify-content-center" style="height: 65px;">
-                                                        <p class=" text-secondary fw-bold " style=" font-size: 14px; margin: 0;"><?= substr($p['nama'], 0, 50); ?>...</p>
-                                                    </div>
-                                                    <p class="text-secondary text-center" style="font-size: 12px; margin: 0;">
-                                                        <del>Rp. <?= number_format($p['harga_min'], 0, ',', '.'); ?></del>
-                                                    </p>
-                                                    <h1 class="text-danger fw-bold text-center" style="font-size: 18px; margin: 0;">
-                                                        <?php if ($p['harga_min'] == $p['harga_max']) : ?>
-                                                            Rp. <?= number_format($p['harga_min'], 0, ',', '.'); ?>
-                                                        <?php else : ?>
-                                                            <?= substr('Rp. ' . number_format($p['harga_min'], 0, ',', '.') . '-' . number_format($p['harga_max'], 0, ',', '.'), 0, 13); ?>...
-                                                        <?php endif ?></h1>
-                                                    <div class="container mt-2">
-                                                        <div class="row justify-items-center">
-                                                            <div class="col">
-                                                                <div class="horizontal-counter">
-                                                                    <button class="btn btn-sm btn-outline-danger rounded-circle" type="button" onclick="decreaseCount(event, this, <?= $p['id_produk']; ?>)"><i class="bi bi-dash"></i></button>
-                                                                    <input type="text" id="counter" class="form-control form-control-sm border-0 text-center" value="1" readonly>
-                                                                    <button class="btn btn-sm btn-outline-danger rounded-circle" type="button" onclick="increaseCount(event, this, <?= $p['id_produk']; ?>)"><i class="bi bi-plus"></i></button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <p class="text-center custom-button " style="display: flex; justify-content: center;">
-                                                        <a href="<?= base_url('produk/' . $p['slug']); ?>?add-to-cart=show" class="btn btn-danger mt-4">
-                                                            <i class="bi bi-cart-plus"></i>
-                                                        </a>
-                                                        <button type="submit" class="btn btn-danger   mx-1 mt-4 fw-bold" data-bs-toggle="modal" data-bs-target="#modalVarianBuy">
-                                                            Beli
-                                                        </button>
-                                                        <span class="badge text-bg-success position-absolute start-0 top-0" style="font-size: 12px; padding: 2px 4px;">10%</span>
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php endforeach; ?>
-                                    <?= $this->include('user/component/scriptAddToCart'); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?= $this->include('user/produk/component/randomProduk'); ?>
             </div>
         </div>
     </div>
