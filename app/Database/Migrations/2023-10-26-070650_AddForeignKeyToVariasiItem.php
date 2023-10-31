0c7a221d9dff26751cadce42abf2070ee547a642
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-10-31 16:16:46 +0700

    Update 2023-10-26-070650_AddForeignKeyToVariasiItem.php
    - Comment

--- app/Database/Migrations/2023-10-26-070650_AddForeignKeyToVariasiItem.php
+++ app/Database/Migrations/2023-10-26-070650_AddForeignKeyToVariasiItem.php
@@ -8,19 +8,19 @@ class AddForeignKeyToVariasiItem extends Migration
 {
     public function up()
     {
-        $this->forge->addColumn('jsf_variasi_item', [
-            'id_produk_batch' => [
-                'type' => 'INT',
-                'constraint' => 11,
-                'unsigned' => true,
-            ],
-        ]);
+        // $this->forge->addColumn('jsf_variasi_item', [
+        //     'id_produk_batch' => [
+        //         'type' => 'INT',
+        //         'constraint' => 11,
+        //         'unsigned' => true,
+        //     ],
+        // ]);
 
-        $this->forge->addForeignKey('id_produk_batch', 'jsf_produk_batch', 'id_produk_batch', true, 'CASCADE');
+        // $this->forge->addForeignKey('id_produk_batch', 'jsf_produk_batch', 'id_produk_batch', true, 'CASCADE');
     }
 
     public function down()
     {
-        $this->forge->dropColumn('jsf_variasi_item', 'id_produk_batch');
+        // $this->forge->dropColumn('jsf_variasi_item', 'id_produk_batch');
     }
 }
