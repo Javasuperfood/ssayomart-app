2940227e29216b2d8edffffbbf2440767b36a9db
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-10-31 13:12:56 +0700

    Update history.php
    - Fix fitur search untuk riwayat transaksi

--- app/Views/user/home/history/history.php
+++ app/Views/user/home/history/history.php
@@ -24,13 +24,13 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
             <?php
             $idTransaksi = null;
             $searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
-            // $foundProducts = false;
+            $foundProducts = false;
 
             foreach ($transaksi as $t) :
                 if ($searchTerm != '' && (strpos(strtolower($t->nama), strtolower($searchTerm)) === false) && (strpos(strtolower($t->sku), strtolower($searchTerm)) === false)) {
                     continue;
                 }
-                // $foundProducts = true;
+                $foundProducts = true;
             ?>
                 <?php if ($idTransaksi != $t->id_checkout) : ?>
                     <div class="row pt-3" onclick="toggleHistory(<?= $t->id_checkout; ?>)">
@@ -127,6 +127,19 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                 <?php endif ?>
                 <?php $idTransaksi = $t->id_checkout ?>
             <?php endforeach; ?>
+            <?php if (!$foundProducts) : // Tampilkan alert jika tidak ditemukan produk 
+            ?>
+                <div class="row pt-3">
+                    <div class="col">
+                        <div class="alert alert-danger rounded border-0" role="alert">
+                            <div class="row">
+                                <div class="col-2"><i class="bi bi-exclamation-diamond-fill text-danger fs-1 position-absolute top-50 start-0 translate-middle-y px-4"></i></div>
+                                <div class="col-10 text-secondary" style="font-size: 15px;">Riwayat Transaksi yang Anda cari tidak ditemukan.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif ?>
 
             <div class="row pb-5">
                 <div class="col"></div>
