f15fe43d1f1ca4ccfbe9a5cceabb1055ece811ff
Gilang Aditia <gaditia744@gmail.com>
2023-11-01 13:10:59 +0700

    update semua view yang belom stabil

--- app/Views/user/home/Kategori.php
+++ app/Views/user/home/Kategori.php
@@ -177,7 +177,7 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                                                 </div>
                                             </a>
                                             <div class="fs-2 mt-2" style="padding: 0 10px 0 10px;">
-                                                <div class="d-flex align-items-start justify-content-center" style="height: 65px;">
+                                                <div class="d-flex text-center align-items-center justify-content-center" style="height: 65px;">
                                                     <p class=" text-secondary fw-bold " style=" font-size: 13px; margin: 0;"><?= substr($p['nama'], 0, 40); ?>...</p>
                                                 </div>
                                                 <p class="text-secondary text-center" style="font-size: 12px; margin: 0;">
