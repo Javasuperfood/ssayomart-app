b352326f3f257334900bdd356e7a989a52755d4d
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-11-02 16:16:05 +0700

    update handle pop up picture
    - Penambahan fungsi agar admin bisa mengatur pop up pada halaman awal aplikasi
    
     note : fungsi tambah, edit, dan hapus sudah done

--- app/Views/dashboard/component/sidebar.php
+++ app/Views/dashboard/component/sidebar.php
@@ -102,6 +102,7 @@
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header text-danger">Menu Banner:</h6>
                 <a class="collapse-item" href="<?= base_url(); ?>dashboard/banner/">List Banner</a>
+                <a class="collapse-item" href="<?= base_url(); ?>dashboard/banner/list-banner">List Banner App</a>
                 <a class="collapse-item" href="<?= base_url(); ?>dashboard/banner/tambah-banner">Tambah Banner</a>
             </div>
         </div>
