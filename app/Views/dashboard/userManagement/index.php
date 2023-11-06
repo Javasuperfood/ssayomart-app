eb8b3c896cce17940818555a396b05911b47dc32
AnaMarisa <ana.marisa.mf@gmail.com>
2023-11-06 16:32:41 +0700

    search and pagination

--- app/Views/dashboard/userManagement/index.php
+++ app/Views/dashboard/userManagement/index.php
@@ -2,22 +2,16 @@
 <?= $this->section('page-content') ?>
 
 <h1 class="h3 mb-2 text-gray-800">User Management</h1>
-<ul class="breadcrumb bg-light">
-    <li class="breadcrumb-item text-danger active">List User Management</li>
-    <li class="breadcrumb-item text-danger"><a class="text-secondary" href="<?= base_url(); ?>dashboard/produk/tambah-produk">Tambah User</a></li>
-</ul>
-<p class="mb-3">Halaman ini dapat menampilkan user management dari ssayomart market disini</p>
 
 <div class="row">
     <div class="col">
-        <a class="btn btn-danger mb-3" href="<?= base_url(); ?>dashboard/produk/tambah-produk"><i class="bi bi-plus-square"></i> Tambah User</a>
         <a class="btn btn-danger mb-3" href="#" data-toggle="modal" data-target="#deleteBatchModal" id="btnDelete" style="display: none;">
             <i class="bi bi-trash-fill"></i>
             Delete
         </a>
     </div>
     <div class="col text-end">
-        <form action="<?= base_url('dashboard/produk'); ?>" method="get">
+        <form action="<?= base_url('dashboard/user-management'); ?>" method="get">
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="search" aria-label="search" name="search" aria-describedby="search">
                 <button class="btn btn-outline-danger" type="submit" id="search">Search</button>
@@ -39,7 +33,6 @@
                             <tr>
                                 <th>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" id="checkAll">
                                         <label class=" form-check-label" for="checkProduk">
                                             No
                                         </label>
@@ -54,10 +47,9 @@
                         </thead>
                         <tbody>
                             <?php
-                            $i = 1;
                             foreach ($users as $user) : ?>
                                 <tr>
-                                    <td><?= $i++; ?></td>
+                                    <td><?= $iterasi++; ?></td>
                                     <td><?= $user['username']; ?></td>
                                     <td><?= $user['telp']; ?></td>
                                     <td><?= $user['active']; ?></td>
@@ -67,6 +59,7 @@
                             <?php endforeach; ?>
                         </tbody>
                     </table>
+                    <?= $pager->links('user', 'pagerS'); ?>
                 </div>
             </div>
         </div>
