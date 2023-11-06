3879c6d5ba64638ffef5be63e8c1b528b1d1a939
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-11-06 15:35:38 +0700

    update validation
    - Penambahan validasi regex agar user tidak bisa menginput spesial karakter untuk halaman tambah alamat, update alamat, update profil

--- app/Database/Seeds/StatusProses.php
+++ app/Database/Seeds/StatusProses.php
@@ -10,25 +10,35 @@ class StatusProses extends Seeder
     {
         $data = [
             [
+                'id_status_pesan'            => 1,
                 'status'        => 'Menunggu Pembayaran',
                 'deskripsi'     => 'Proses tertunda karena pembeli belum melakukan transaksi',
             ],
             [
+                'id_status_pesan'            => 2,
                 'status'        => 'Paket Diproses',
                 'deskripsi'     => 'Paket sudah diproses oleh cabang terdekat',
             ],
             [
+                'id_status_pesan'            => 3,
                 'status'        => 'Paket Dikirim',
                 'deskripsi'     => 'Paket sudah dalam proses pengiriman',
             ],
             [
+                'id_status_pesan'            => 4,
                 'status'        => 'Paket Terkirim',
                 'deskripsi'     => 'Paket sudah diterima oleh pembeli',
             ],
             [
+                'id_status_pesan'            => 5,
                 'status'        => 'Gagal',
                 'deskripsi'     => 'Transaksi pemabayaran gagal',
             ],
+            // [
+            // 'id_status_pesan'            => 6,
+            //     'status'        => 'Dibatalkan',
+            //     'deskripsi'     => 'Pesanan Dibatalkan',
+            // ],
         ];
 
         $this->db->table('jsf_status_pesan')->insertBatch($data);
