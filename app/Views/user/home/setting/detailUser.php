7a150f23fce1038f6e8201ec7b44ef01500eb188
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-11-09 11:44:37 +0700

    update
    - Perbaikan modal box hapus profil

--- app/Views/user/home/setting/detailUser.php
+++ app/Views/user/home/setting/detailUser.php
@@ -36,15 +36,16 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="exampleModalLabel">احذف حسابك هل أنت متأكد؟</h1>
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi Penghapusan Akun</h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                Antum yakin mau hapus akun banyak promosi gocujang disini loh
+                                                Akun anda akan dihapus oleh Admin.
+                                                Apakah anda yakin untuk mengajukan penghapusan akun?
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                                <button type="button" class="btn btn-danger">Ya</button>
                                             </div>
                                         </div>
                                     </div>
