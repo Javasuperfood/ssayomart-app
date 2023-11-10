67ea02ebc7c98fbd4734a88241b7284d2895d157
kikuKeii <miftakhuddinfalaki@gmail.com>
2023-11-10 08:58:30 +0700

    update

---
+++ app/Filters/Env.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\HTTP\IncomingRequest;
+
+class Env implements FilterInterface
+{
+    /**
+     * Do whatever processing this filter needs to do.
+     * By default it should not return anything during
+     * normal execution. However, when an abnormal state
+     * is found, it should return an instance of
+     * CodeIgniter\HTTP\Response. If it does, script
+     * execution will end and that Response will be
+     * sent back to the client, allowing for error pages,
+     * redirects, etc.
+     *
+     * @param RequestInterface $request
+     * @param array|null       $arguments
+     *
+     * @return mixed
+     */
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (!$request->hasHeader('X-Environment')) {
+            return $request;
+        }
+        $env = $request->getHeaderLine('X-Environment');
+        // Lakukan validasi sesuai dengan kebutuhan
+        if ($env === 'development') {
+            putenv("CI_ENVIRONMENT=$env");
+            return $request;
+        }
+        return $request;
+    }
+
+    /**
+     * Allows After filters to inspect and modify the response
+     * object as needed. This method does not allow any way
+     * to stop execution of other after filters, short of
+     * throwing an Exception or Error.
+     *
+     * @param RequestInterface  $request
+     * @param ResponseInterface $response
+     * @param array|null        $arguments
+     *
+     * @return mixed
+     */
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        //
+    }
+}
