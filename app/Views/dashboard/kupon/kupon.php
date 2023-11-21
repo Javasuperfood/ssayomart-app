66bfb019073769c7724867c2f6b622db698c1231
Harun_fernando <harunfernando12@gmail.com>
2023-11-21 10:02:52 +0700

    Revisi Dashboard Kupon

--- app/Views/dashboard/kupon/kupon.php
+++ app/Views/dashboard/kupon/kupon.php
@@ -2,22 +2,23 @@
 <?= $no = 1; ?>
 <?= $this->section('page-content') ?>
 
-<h1 class="h3 mb-2 text-gray-800">Manajemen Kupon</h1>
-<ul class="breadcrumb bg-light">
-    <li class="breadcrumb-item text-danger active">List Kupon</li>
+<h1 class="h3 mb-3 text-gray-800">Manajemen Kupon</h1>
+<ul class="breadcrumb bg-light px-0">
+    <li class="breadcrumb-item text-danger active text-decoration-underline">List Kupon</li>
     <li class="breadcrumb-item text-danger"><a class="text-secondary" href="<?= base_url(); ?>dashboard/kupon/tambah-kupon">Tambah Kupon</a></li>
 </ul>
 
-<a class="btn btn-danger mb-3" href="<?= base_url(); ?>dashboard/kupon/tambah-kupon"><i class="bi bi-plus-circle-fill"></i> Tambah kupon</a>
+<a class="btn btn-danger mb-4" href="<?= base_url(); ?>dashboard/kupon/tambah-kupon"><i class="bi bi-plus-circle me-2"></i>Tambah kupon</a>
 
 <!-- DataTales Example -->
-<div class="card border-0 shadow-sm mb-4">
-    <div class="card-header border-0 py-3">
-        <h6 class="m-0 font-weight-bold">List Kupon</h6>
+<div class="card border-1 shadow-sm mb-5">
+    <div class="card-header d-flex justify-content-start align-items-center border-1 py-3">
+        <i class="bi bi-file-text-fill"></i>
+        <h6 class="m-0 font-weight-bold px-2">List Kupon</h6>
     </div>
-    <div class="card-body ">
+    <div class="card-body mt-2">
         <div class="table-responsive">
-            <table class=" table table-borderless text-center" id="dataTable" width="100%" cellspacing="0">
+            <table class=" table text-center" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
                         <th>No</th>
@@ -34,14 +35,14 @@
                 <tbody>
                     <?php foreach ($kupon_Model as $kp) : ?>
                         <tr>
-                            <td><?= $no++; ?></td>
-                            <td><?= $kp['nama']; ?></td>
-                            <td><?= $kp['kode']; ?></td>
-                            <td><?= $kp['deskripsi']; ?></td>
-                            <td><?= $kp['discount']; ?></td>
-                            <td><?= 'Rp ' . number_format($kp['total_buy'], 0, ',', '.'); ?></td>
-                            <td><?= date("d-m-Y", strtotime($kp['is_active'])); ?></td>
-                            <td><?= $kp['username']; ?></td>
+                            <td class="align-middle"><?= $no++; ?></td>
+                            <td class="align-middle"><?= $kp['nama']; ?></td>
+                            <td class="align-middle"><?= $kp['kode']; ?></td>
+                            <td class="align-middle"><?= $kp['deskripsi']; ?></td>
+                            <td class="align-middle"><?= $kp['discount']; ?></td>
+                            <td class="align-middle"><?= 'Rp ' . number_format($kp['total_buy'], 0, ',', '.'); ?></td>
+                            <td class="align-middle"><?= date("d-m-Y", strtotime($kp['is_active'])); ?></td>
+                            <td class="align-middle"><?= $kp['username']; ?></td>
                             <td class="text-center">
                                 <div class="nav-item dropdown no-arrow">
                                     <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
--- app/Views/dashboard/kupon/tambahKupon.php
+++ app/Views/dashboard/kupon/tambahKupon.php
@@ -1,15 +1,18 @@
 <?= $this->extend('dashboard/dashboard') ?>
 <?= $this->section('page-content') ?>
 
-<div class="card border-0 shadow-sm position-relative">
-    <div class="card-header border-0 py-3">
-        <h6 class="m-0 font-weight-medium">Masukan Kupon Baru</h6>
+<h1 class="h3 mb-3 text-gray-800">Tambah Kupon</h1>
+
+<div class="card border-1 shadow-sm position-relative mb-5">
+    <div class="card-header d-flex justify-content-start align-items-center border-1 py-3">
+        <i class="bi bi-file-earmark-plus-fill"></i>
+        <h6 class="m-0 fw-bold px-2">Masukan Kupon Baru</h6>
     </div>
     <div class="card-body">
         <!-- code -->
         <form action="<?= base_url(); ?>dashboard/kupon/tambah-kupon/save" method="post">
             <?= csrf_field(); ?>
-            <div class="mb-3">
+            <div class="mb-4">
                 <div class="form-check">
                     <input class="form-check-input bg-danger" type="checkbox" value="1" name="is_active" id="isActive" <?= (old('is_active') == 1) ? 'checked' : ''; ?>>
                     <label class="form-check-label" for="isActive">
@@ -17,26 +20,26 @@
                     </label>
                 </div>
             </div>
-            <div class="mb-3">
-                <label for="nama_kupon" class="form-label">Judul Kupon <span class="text-danger fs-5">*</span></label>
-                <input type="text" class="form-control <?= (validation_show_error('nama')) ? 'is-invalid' : 'border-0'; ?> shadow-sm" id="nama_kupon" name="nama_kupon" placeholder="Nama Kupon Anda" value="<?= old('nama_kupon') ?>">
+            <div class="mb-4">
+                <label for="nama_kupon" class="form-label">Judul Kupon<span class="text-danger fs-5">*</span></label>
+                <input type="text" class="form-control <?= (validation_show_error('nama')) ? 'is-invalid' : 'border-1'; ?>" id="nama_kupon" name="nama_kupon" placeholder="Nama Kupon Anda" value="<?= old('nama_kupon') ?>">
                 <div class="invalid-feedback"><?= validation_show_error('nama'); ?></div>
             </div>
-            <label for="kode_kupon" class="form-label">Kode Reveral Kupon <span class="text-danger fs-5">*</span></label>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control <?= (validation_show_error('kode')) ? 'is-invalid' : 'border-0'; ?> shadow-sm" id="kode_kupon" name="kode_kupon" placeholder="Kode Kupon" value="<?= old('kode_kupon') ?>" aria-describedby="generateKode">
-                <button class="btn btn-outline-danger shadow-sm" id="generateKode" type="button">Dapatkan Kode</button>
+            <label for="kode_kupon" class="form-label">Kode Reveral Kupon<span class="text-danger fs-5">*</span></label>
+            <div class="input-group mb-4">
+                <input type="text" class="form-control <?= (validation_show_error('kode')) ? 'is-invalid' : 'border-1'; ?>" id="kode_kupon" name="kode_kupon" placeholder="Kode Kupon" value="<?= old('kode_kupon') ?>" aria-describedby="generateKode">
+                <button class="btn btn-danger" id="generateKode" type="button">Dapatkan Kode</button>
             </div>
             <div class="invalid-feedback"><?= validation_show_error('kode'); ?></div>
-            <div class="mb-3">
-                <label for="deskripsi_kupon" class="form-label">Deskripsi Kupon <span class="text-danger fs-5">*</span></label>
-                <textarea class="form-control <?= (validation_show_error('deskripsi')) ? 'is-invalid' : 'border-0'; ?> shadow-sm" id="deskripsi_kupon" name="deskripsi_kupon" rows="3" placeholder="Deskripsi Kupon ...." value=""><?= old('deskripsi_kupon') ?></textarea>
+            <div class="mb-4">
+                <label for="deskripsi_kupon" class="form-label">Deskripsi Kupon<span class="text-danger fs-5">*</span></label>
+                <textarea class="form-control <?= (validation_show_error('deskripsi')) ? 'is-invalid' : 'border-1'; ?>" id="deskripsi_kupon" name="deskripsi_kupon" rows="3" placeholder="Deskripsi Kupon ...." value=""><?= old('deskripsi_kupon') ?></textarea>
                 <div class="invalid-feedback"><?= validation_show_error('deskripsi'); ?></div>
             </div>
 
-            <div class="mb-3">
-                <label for="" class="form-label">Potongan Harga Kupon <span class="text-danger fs-5">*</span></label>
-                <select class="form-select <?= (validation_show_error('discount')) ? 'is-invalid' : 'border-0'; ?> shadow-sm" name="discount" id="">
+            <div class="mb-4">
+                <label for="" class="form-label">Potongan Harga Kupon<span class="text-danger fs-5">*</span></label>
+                <select class="form-select <?= (validation_show_error('discount')) ? 'is-invalid' : 'border-1'; ?>" name="discount" id="">
                     <option value="" selected>Pilih discount</option>
                     <?php for ($i = 5; $i <= 100; $i += 5) : ?>
                         <option value="<?= $i / 100; ?>" <?= (old('discount') == $i / 100) ? 'selected' : ''; ?>><?= $i; ?>%</option>
@@ -44,22 +47,23 @@
                 </select>
                 <div class="invalid-feedback"><?= validation_show_error('discount'); ?></div>
             </div>
-            <div class="mb-3">
+            <div class="mb-4">
                 <div class="row">
                     <div class="col-md-6">
-                        <label for="total_buy" class="form-label">Minimal Pembelian <span class="text-danger fs-5">*</span></label>
-                        <input type="text" class="form-control <?= (validation_show_error('total_buy')) ? 'is-invalid' : 'border-0'; ?> shadow-sm" id="total_buy" name="total_buy" placeholder="total pembelian" value="<?= old('total_buy') ?>" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');">
+                        <label for="total_buy" class="form-label">Minimal Pembelian<span class="text-danger fs-5">*</span></label>
+                        <input type="text" class="form-control <?= (validation_show_error('total_buy')) ? 'is-invalid' : 'border-1'; ?>" id="total_buy" name="total_buy" placeholder="total pembelian" value="<?= old('total_buy') ?>" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');">
                         <div class="invalid-feedback"><?= validation_show_error('total_buy'); ?></div>
                     </div>
                     <div class="col-md-6">
-                        <label for="" class="form-label">Maksimal Digunakan <span class="text-danger fs-5">*</span></label>
-                        <input type="number" class="form-control <?= (validation_show_error('available_kupon')) ? 'is-invalid' : 'border-0'; ?> shadow-sm" name="available_kupon" value="<?= (old('available_kupon')) ? old('available_kupon') : 1; ?>" id="" placeholder="Mkasimal digunakan">
+                        <label for="" class="form-label">Maksimal Digunakan<span class="text-danger fs-5">*</span></label>
+                        <input type="number" class="form-control <?= (validation_show_error('available_kupon')) ? 'is-invalid' : 'border-1'; ?>" name="available_kupon" value="<?= (old('available_kupon')) ? old('available_kupon') : 1; ?>" id="" placeholder="Mkasimal digunakan">
                         <div class="invalid-feedback"><?= validation_show_error('available_kupon'); ?></div>
                     </div>
                 </div>
             </div>
-            <div class="mb-3 d-flex justify-content-center">
-                <button type="submit" class="btn btn-danger mt-3">Tambah Kupon</button>
+            <hr class="my-4" style="border-width: 1px; border-color: #d1d3e2; border-style: solid;">
+            <div class="d-flex justify-content-end">
+                <button type="submit" class="btn btn-danger">Tambah Kupon</button>
             </div>
         </form>
     </div>
