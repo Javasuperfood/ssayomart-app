828b62e5d6fbecdae548d4767e6dcbc9c917ecea
ramadhani <rizqiramadhani37@gmail.com>
2023-12-01 14:06:28 +0700

    tambah fitur ewallet desktop done

--- app/Views/user/home/payment/ewallet.php
+++ app/Views/user/home/payment/ewallet.php
@@ -13,11 +13,8 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
         <div class="container mt-5">
             <div class="row">
                 <div class="col">
-
                     <p class="fs-3 text-center">Recomendasi E-Wallet</p>
                     <hr class="border-darker mt-0 mb-3">
-
-
                     <div style="height:80px;" class="card border-0 shadow-sm d-flex justify-content-center align-items-start">
                         <div class="icons">
                             <input type="radio" name="emoney" value="dana" class="mx-2">
@@ -32,15 +29,12 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                             <span class="text-secondary">saldo Rp.1.000.000</span>
                         </div>
                     </div>
-
-
                 </div>
             </div>
             <div class="row mt-3">
                 <div class="col">
                     <p class="fs-3 text-center">E-Wallet lainnya</p>
                     <hr class="border-darker mt-0 mb-3">
-
                     <div style="height:80px;" class="card border-0 shadow-sm d-flex justify-content-center align-items-start">
                         <div class="icons">
                             <input type="radio" name="emoney" value="dana" class="mx-2">
@@ -67,8 +61,6 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
             </div>
         </div>
     </div>
-    </div>
-    </div>
     <style>
         .border-darker {
             border-color: red;
@@ -79,8 +71,65 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
 
 <?php else : ?>
     <!-- Desktop View -->
-    <div id="desktopContent" style="margin-top:50px;">
+    <div id="desktopContent" style="margin-top:150px;">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <p class="fs-3 text-center">Recomendasi E-Wallet</p>
+                    <hr class="border-darker mt-0 mb-3">
+                    <div style="height:80px;" class="card border-0 shadow-sm d-flex justify-content-center align-items-start">
+                        <div class="icons">
+                            <input type="radio" name="emoney" value="dana" class="mx-2">
+                            <img src="<?= base_url() ?>assets/img/payment/dana.png" class="my-2 mx-2" width="50">
+                            <span class="text-secondary">saldo Rp.1.000.000</span>
+                        </div>
+                    </div>
+                    <div style="height:80px;" class="mt-2 card border-0 shadow-sm d-flex justify-content-center align-items-start">
+                        <div class="icons">
+                            <input type="radio" name="emoney" value="dana" class="mx-2">
+                            <img src="<?= base_url() ?>assets/img/payment/ovo.png" class="my-2 mx-2" width="30">
+                            <span class="text-secondary">saldo Rp.1.000.000</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <p class="fs-3 text-center">E-Wallet lainnya</p>
+                    <hr class="border-darker mt-0 mb-3">
+                    <div style="height:80px;" class="card border-0 shadow-sm d-flex justify-content-center align-items-start">
+                        <div class="icons">
+                            <input type="radio" name="emoney" value="dana" class="mx-2">
+                            <img src="<?= base_url() ?>assets/img/payment/Gopay.png" class="my-2 mx-2" width="80">
+                            <span class="text-secondary">saldo Rp.1.000.000</span>
+                        </div>
+                    </div>
+                    <div style="height:80px;" class="mt-2 card border-0 shadow-sm d-flex justify-content-center align-items-start">
+                        <div class="icons">
+                            <input type="radio" name="emoney" value="dana" class="mx-2">
+                            <img src="<?= base_url() ?>assets/img/payment/ShopeePay.png" class="my-2 mx-2" width="80">
+                            <span class="text-secondary">saldo Rp.1.000.000</span>
+                        </div>
+                    </div>
+                    <div style="height:80px;" class="mt-2 card border-0 shadow-sm d-flex justify-content-center align-items-start">
+                        <div class="icons">
+                            <input type="radio" name="emoney" value="dana" class="mx-2">
+                            <img src="<?= base_url() ?>assets/img/payment/linkaja.png" class="my-2 mx-2" width="30">
+                            <span class="text-secondary">saldo Rp.1.000.000</span>
+                        </div>
+                    </div>
+                    <div class="btn btn-danger d-block mt-4">PAY <span class="fas fa-dollar-sign ms-2"></span>Rp 15.000.000<span class="ms-3 fas fa-arrow-right"></span></div>
+                </div>
+            </div>
+        </div>
     </div>
+    <style>
+        .border-darker {
+            border-color: red;
+            border-width: 2px;
+            font-weight: bold;
+        }
+    </style>
 <?php endif; ?>
 <!-- End Desktop View -->
 <?php
