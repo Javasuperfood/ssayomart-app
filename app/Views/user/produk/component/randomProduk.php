8c4b2a14ba56ee5fb5de1ad7460378ee0f0347c5
Gilang Aditia <gaditia744@gmail.com>
2023-12-04 08:36:07 +0700

    update

--- app/Views/user/produk/component/randomProduk.php
+++ app/Views/user/produk/component/randomProduk.php
@@ -14,13 +14,13 @@
                                 </a>
                                 <div class="fs-2 mt-2" style="padding: 0 10px 0 10px;">
                                     <div class="d-flex align-items-start justify-content-center" style="height: 65px;">
-                                        <p class=" text-secondary fw-bold " style=" font-size: 12px; margin: 0;"><?= substr($p['nama'], 0, 40); ?></p>
+                                        <p class=" text-secondary fw-bold " style=" font-size: 10px; margin: 0;"><?= substr($p['nama'], 0, 30); ?></p>
                                     </div>
-                                    <p class="text-secondary" style="font-size: 10px; margin: 0;">
+                                    <p class="text-secondary" style="font-size: 8px; margin: 0;">
                                         <del>Rp. <?= number_format($p['harga_min'], 0, ',', '.'); ?></del>
                                     </p>
 
-                                    <h1 class="text-danger fw-bold mt-1 pb-3" style="font-size: 14px; margin: 0;">
+                                    <h1 class="text-danger fw-bold mt-1 pb-3" style="font-size: 10px; margin: 0;">
                                         <?php if ($p['harga_min'] == $p['harga_max']) : ?>
                                             Rp. <?= number_format($p['harga_min'], 0, ',', '.'); ?>
                                         <?php else : ?>
@@ -127,7 +127,7 @@
     }
 
     .icon {
-        font-size: 14px;
+        font-size: 18px;
         color: #e83b2e;
         transition: all 0.3s ease;
         cursor: pointer;
