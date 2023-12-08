581216567b6ab7ee53e3a5c55b5fce7440d316b7
kikuKeii <miftakhuddinfalaki@gmail.com>
2023-12-08 09:18:19 +0700

    update codeigniter 4.3.8 to 4.4.0

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,40 +1,12 @@
 <?php
 
-namespace Config;
+use CodeIgniter\Router\RouteCollection;
 
-use App\Controllers\GoSendController;
-use App\Controllers\Kategori;
-
-// Create a new instance of our RouteCollection class.
-$routes = Services::routes();
-
-/*
- * --------------------------------------------------------------------
- * Router Setup
- * --------------------------------------------------------------------
- */
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-// $routes->set404Override(fn () => view('404', ['title' => '404']));
-$routes->set404Override();
-// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
-// where controller filters or CSRF protection are bypassed.
-// If you don't want to define all routes, please use the Auto Routing (Improved).
-// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-// $routes->setAutoRoute(false);
-
-/*
- * --------------------------------------------------------------------
- * Route Definitions
- * --------------------------------------------------------------------
+/**
+ * @var RouteCollection $routes
  */
 
-
-
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
+service('auth')->routes($routes);
 
 $routes->get('/', 'KategoriController::index');
 $routes->get('lang/{locale}', 'Language::index');
@@ -334,7 +306,6 @@ $routes->group('dashboard', ['filter' => 'group:admin,superadmin'], static funct
     $routes->get('admin-management', 'AdminManagementController::index');
 });
 
-service('auth')->routes($routes);
 
 $routes->group('api', static function ($routes) { //nanti tambahkan filter auth via Tooken
     // $routes->resource('kategori');
@@ -396,20 +367,3 @@ $routes->group('/webhook', ['filter' => 'webhookFilter'], static function ($rout
     $routes->post('/', 'NotifController::sendOrderNotification');
     $routes->post('for_warehouse_notification', 'NotifController::warehouseGosendNotification');
 });
-
-/*
- * --------------------------------------------------------------------
- * Additional Routing
- * --------------------------------------------------------------------
- *
- * There will often be times that you need additional routing and you
- * need it to be able to override any defaults in this file. Environment
- * based routes is one such time. require() additional route files here
- * to make that happen.
- *
- * You will have access to the $routes object within that file without
- * needing to reload it.
- */
-if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
-    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
-}
--- app/Views/404.php
+++ app/Views/404.php
@@ -1,1 +1,70 @@
-<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title><?= $title; ?></title><style>@import url("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css");@import url("https://fonts.googleapis.com/css?family=Arvo");.page_404{padding:40px 0;background:#fff;font-family:'Arvo',serif;}.page_404 img{width:100%;}.four_zero_four_bg{background-image:url(<?= base_url('assets/gif/404.gif'); ?>);height:400px;background-position:center;}.four_zero_four_bg h1{font-size:80px;}.four_zero_four_bg h3{font-size:80px;}.link_404{color:#fff!important;padding:10px 20px;background:#ec2614;margin:20px 0;display:inline-block;}.contant_box_404{margin-top:-50px;}</style></head><body><section class="page_404"><div class="container"><div class="row"><div class="col-sm-12"><div class="col-sm-10 col-sm-offset-1 text-center"><div class="four_zero_four_bg"><h1 class="text-center">404</h1></div><div class="contant_box_404"><h3 class="h2">Look like you're lost</h3><p>the page you are looking for not available!</p><a href="<?= base_url(); ?>" class="link_404">Go to Home</a></div></div></div></div></div></section></body></html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>404 Not Found</title>
+    <style>
+        @import url("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css");
+        @import url("https://fonts.googleapis.com/css?family=Arvo");
+
+        .page_404 {
+            padding: 40px 0;
+            background: #fff;
+            font-family: 'Arvo', serif;
+        }
+
+        .page_404 img {
+            width: 100%;
+        }
+
+        .four_zero_four_bg {
+            background-image: url(<?= base_url('assets/gif/404.gif'); ?>);
+            height: 400px;
+            background-position: center;
+        }
+
+        .four_zero_four_bg h1 {
+            font-size: 80px;
+        }
+
+        .four_zero_four_bg h3 {
+            font-size: 80px;
+        }
+
+        .link_404 {
+            color: #fff !important;
+            padding: 10px 20px;
+            background: #ec2614;
+            margin: 20px 0;
+            display: inline-block;
+        }
+
+        .contant_box_404 {
+            margin-top: -50px;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="page_404">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="col-sm-10 col-sm-offset-1 text-center">
+                        <div class="four_zero_four_bg">
+                            <h1 class="text-center">404</h1>
+                        </div>
+                        <div class="contant_box_404">
+                            <h3 class="h2">Look like you're lost</h3>
+                            <p>the page you are looking for not available!</p><a href="<?= base_url(); ?>" class="link_404">Go to Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
