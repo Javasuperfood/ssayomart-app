ff72135841e47a41f662a9cd0badbaa47b0548a8
Nama Anda <email@example.com>
2023-12-13 11:24:28 +0700

    add iframe di accordion

--- app/Views/dashboard/panduan/panduanAplikasi.php
+++ app/Views/dashboard/panduan/panduanAplikasi.php
@@ -15,75 +15,138 @@
       <div class="row">
         <div class="col mx-auto">
           <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
+            <div class="mb-3">
+                  <div class="accordion-item">
               <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Accordion Item #1</button>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Dashboard</button>
               </h2>
               <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                   <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
                   well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
                   <code>.accordion-body</code>, though the transition does limit overflow.
+                   <div class="embed-responsive embed-responsive-16by9 mt-3">
+         <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
                 </div>
               </div>
             </div>
+            </div>
+        
 
           <!-- Accordion Item #2 -->
-<div class="accordion-item">
+          <div class="mb-3"><div class="accordion-item mb-3">
   <h2 class="accordion-header" id="panduanAplikasiHeadingTwo">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseTwo" aria-expanded="false" aria-controls="panduanAplikasiCollapseTwo">Accordion Item #2</button>
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseTwo" aria-expanded="false" aria-controls="panduanAplikasiCollapseTwo">Report</button>
   </h2>
   <div id="panduanAplikasiCollapseTwo" class="accordion-collapse collapse" aria-labelledby="panduanAplikasiHeadingTwo" data-bs-parent="#accordionExample">
     <div class="accordion-body">
        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
                   well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
                   <code>.accordion-body</code>, though the transition does limit overflow.
+                           <div class="embed-responsive embed-responsive-16by9 mt-3">
+         <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
     </div>
   </div>
 </div>
+</div>
+
 
 <!-- Accordion Item #3 -->
-<div class="accordion-item">
+<div class="mb-3"><div class="accordion-item mb-3">
   <h2 class="accordion-header" id="panduanAplikasiHeadingThree">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseThree" aria-expanded="false" aria-controls="panduanAplikasiCollapseThree">Accordion Item #3</button>
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseThree" aria-expanded="false" aria-controls="panduanAplikasiCollapseThree">Pesanan</button>
   </h2>
   <div id="panduanAplikasiCollapseThree" class="accordion-collapse collapse" aria-labelledby="panduanAplikasiHeadingThree" data-bs-parent="#accordionExample">
     <div class="accordion-body">
      <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
                   well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
                   <code>.accordion-body</code>, though the transition does limit overflow.
+                           <div class="embed-responsive embed-responsive-16by9 mt-3">
+            <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
     </div>
   </div>
-</div>
+</div></div>
+
 
 
             <!-- Accordion Item #4 -->
-            <div class="accordion-item">
+            <div class="mb-3">
+                <div class="accordion-item mb-3">
               <h2 class="accordion-header" id="headingFour">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Accordion Item #4</button>
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Produk Menu</button>
               </h2>
               <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                   <strong>This is the fourth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
                   well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
                   <code>.accordion-body</code>, though the transition does limit overflow.
+                           <div class="embed-responsive embed-responsive-16by9 mt-3">
+          <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
                 </div>
               </div>
             </div>
+            </div>
+          
 
             <!-- Accordion Item #5 -->
-            <div class="accordion-item">
+            <div class="mb-3">
+               <div class="accordion-item ">
               <h2 class="accordion-header" id="headingFive">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Accordion Item #5</button>
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Kategori Menu</button>
               </h2>
               <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                   <strong>This is the fifth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
                   well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
                   <code>.accordion-body</code>, though the transition does limit overflow.
+                           <div class="embed-responsive embed-responsive-16by9 mt-3">
+          <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        
                 </div>
+                
               </div>
+              
             </div>
+            </div>
+           <!-- Accordion Item #6 -->
+<div class="mb-3">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSix">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">Management Konten</button>
+    </h2>
+    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the sixth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <div class="embed-responsive embed-responsive-16by9 mt-3">
+          <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Accordion Item #7 -->
+<div class="mb-3">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSeven">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">Kupon</button>
+    </h2>
+    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the seventh item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <div class="embed-responsive embed-responsive-16by9 mt-3">
+          <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
           </div>
         </div>
       </div>
