4ea899e7ba4d6153f51b618500488c040dce7e9b
kikuKeii <miftakhuddinfalaki@gmail.com>
2023-12-14 10:22:32 +0700

    update

--- app/Config/WebHook.php
+++ app/Config/WebHook.php
@@ -15,7 +15,7 @@ class WebHook extends BaseConfig
         if ($isP == 'production') {
             $this->base_url = 'https://kilat-api.gojekapi.com/';
             $this->client_id = 'ssayomart-engine';
-            $this->pas_key = 'caffa1988f9930ca7c66747e892a7e689f3ded81b3d142b10fc99df7ff36f989';
+            $this->pas_key = '03085d71b11d9b9fb76a421af9e56b8b82997020105b039d89b4f04d3cfbbcfe';
         } elseif ($isP == 'development') {
             $this->base_url = 'https://integration-kilat-api.gojekapi.com';
             $this->client_id = 'ssayomart-engine';
