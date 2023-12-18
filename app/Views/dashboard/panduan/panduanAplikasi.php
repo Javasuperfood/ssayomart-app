1793066f5d47178440ea62e9773f6da8a8bc1d03
Nama Anda <email@example.com>
2023-12-18 09:48:46 +0700

    styling acordion panduan aplikasi

--- app/Views/dashboard/panduan/panduanAplikasi.php
+++ app/Views/dashboard/panduan/panduanAplikasi.php
@@ -15,10 +15,11 @@
       <div class="row">
         <div class="col mx-auto">
           <div class="accordion" id="accordionExample">
+            <!-- Accordion Dashboard -->
             <div class="mb-3">
                   <div class="accordion-item">
               <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Dashboard</button>
+                <button class="accordion-button bg-danger border-danger text-white fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Dashboard</button>
               </h2>
               <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
@@ -36,10 +37,10 @@
             </div>
         
 
-          <!-- Accordion Item #2 -->
+          <!-- Accordion Report #2 -->
           <div class="mb-3"><div class="accordion-item mb-3">
   <h2 class="accordion-header" id="panduanAplikasiHeadingTwo">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseTwo" aria-expanded="false" aria-controls="panduanAplikasiCollapseTwo">Report</button>
+    <button class="accordion-button collapsed bg-danger border-danger text-white fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseTwo" aria-expanded="false" aria-controls="panduanAplikasiCollapseTwo">Report</button>
   </h2>
   <div id="panduanAplikasiCollapseTwo" class="accordion-collapse collapse" aria-labelledby="panduanAplikasiHeadingTwo" data-bs-parent="#accordionExample">
     <div class="accordion-body">
@@ -58,16 +59,23 @@
 </div>
 
 
-<!-- Accordion Item #3 -->
+<!-- Accordion Pesanan #3 -->
 <div class="mb-3"><div class="accordion-item mb-3">
   <h2 class="accordion-header" id="panduanAplikasiHeadingThree">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseThree" aria-expanded="false" aria-controls="panduanAplikasiCollapseThree">Pesanan</button>
+    <button class="accordion-button collapsed bg-danger border-danger text-white fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#panduanAplikasiCollapseThree" aria-expanded="false" aria-controls="panduanAplikasiCollapseThree">Pesanan</button>
   </h2>
   <div id="panduanAplikasiCollapseThree" class="accordion-collapse collapse" aria-labelledby="panduanAplikasiHeadingThree" data-bs-parent="#accordionExample">
     <div class="accordion-body">
-     <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
-                  well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-                  <code>.accordion-body</code>, though the transition does limit overflow.
+    
+                 <ul>
+                  <p>Pada menu pesanan terdapat enam submenu, yaitu:</p>
+                  <li>Semua Pesanan</li>
+                   <li>Menunggu Pembayaran</li>
+                    <li>Dalam Proses</li>
+                     <li>Dalam Perjalanan</li>
+                      <li>Pesanan Terkirim</li>
+                       <li>Refund</li>
+                 </ul>
                            <div class="embed-responsive embed-responsive-16by9 mt-3">
             <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
@@ -77,17 +85,28 @@
 
 
 
-            <!-- Accordion Item #4 -->
+            <!-- Accordion Produk Menu #4 -->
             <div class="mb-3">
                 <div class="accordion-item mb-3">
               <h2 class="accordion-header" id="headingFour">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Produk Menu</button>
+                <button class="accordion-button collapsed bg-danger border-danger text-white fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Produk Menu</button>
               </h2>
               <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the fourth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
-                  well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-                  <code>.accordion-body</code>, though the transition does limit overflow.
+              <ul>
+                <p>Menu Produk<p>
+                <li>List Produk</li>
+                <li>Tambah Produk</li>
+                <li>Produk Kategori-Batch</li>
+                <li>Tambah Variasi</li>
+                <li>Fetching Produk</li>
+              </ul>
+              <ul>
+                <p>Menu Promosi</p>
+                <li>Buat Promosi</li>
+                <li>Tambah Promosi Produk</li>
+                <li>Tambah Promosi Produk Batch</li>
+              </ul>
                            <div class="embed-responsive embed-responsive-16by9 mt-3">
           <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
@@ -97,17 +116,19 @@
             </div>
           
 
-            <!-- Accordion Item #5 -->
+            <!-- Accordion Kategori Menu #5 -->
             <div class="mb-3">
                <div class="accordion-item ">
               <h2 class="accordion-header" id="headingFive">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Kategori Menu</button>
+                <button class="accordion-button collapsed bg-danger border-danger text-white fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Kategori Menu</button>
               </h2>
               <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the fifth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as
-                  well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-                  <code>.accordion-body</code>, though the transition does limit overflow.
+                 <ul>
+                  <li>List Kategori</li>
+                  <li>Tambah Kategori</li>
+                  <li> Ubah Urutan Kategori</li>
+                 </ul>
                            <div class="embed-responsive embed-responsive-16by9 mt-3">
           <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
@@ -118,15 +139,20 @@
               
             </div>
             </div>
-           <!-- Accordion Item #6 -->
+           <!-- Accordion Management Kontent #6 -->
 <div class="mb-3">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingSix">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">Management Konten</button>
+      <button class="accordion-button collapsed bg-danger border-danger text-white fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">Management Konten</button>
     </h2>
     <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the sixth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      <p>dalam menu management konten terdapat dua sub menu yaitu:</p>
+      <ul>
+
+      <li>Banner</li>
+      <li>Artikel</li>
+      </ul>
         <div class="embed-responsive embed-responsive-16by9 mt-3">
           <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
@@ -135,15 +161,19 @@
   </div>
 </div>
 
-<!-- Accordion Item #7 -->
+<!-- Accordion Kupon #7 -->
 <div class="mb-3">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingSeven">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">Kupon</button>
+      <button class="accordion-button bg-danger border-danger text-white fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">Kupon</button>
     </h2>
     <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the seventh item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+     <ul>
+      <p>Pada menu kupon, terdapat tabel dan tombol "Tambah Kupon"</p>
+      <li>Tabel menampilkan data produk yang sebelumnya telah dibuatkan kupon.</li>
+      <li>Tombol "Tambah Kupon" digunakan untuk menambahkan kupon baru dengan mengisi judul kupon, kode referal kupon, deskripsi kupon, memilih diskon, total pembelian, dan maksimal penggunaan. Setelah itu, terdapat tombol "Tambah Kupon" untuk menambahkan kupon yang telah dibuat dan ditampilkan ke dalam tabel kupon.</li>
+     </ul>
         <div class="embed-responsive embed-responsive-16by9 mt-3">
           <iframe class="rounded" width="560" height="315" src="https://www.youtube.com/embed/okP1yx39zSM?si=csbbpXi9uEKzaZB2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
