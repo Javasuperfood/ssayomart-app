dfabcd8daa91fb3d1a92617d9aff8ca8d1cec2e6
ramadhani <rizqiramadhani37@gmail.com>
2023-12-21 11:41:41 +0700

    solving button navbar cart done

--- app/Views/user/home/component/navbarBottom.php
+++ app/Views/user/home/component/navbarBottom.php
@@ -9,16 +9,16 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
 <?php if ($isMobile) : ?>
     <div id="mobileContent">
         <nav class="navbar navbar-expand fixed-bottom shadow-sm rounded-top-4" style="height: 55px; background-color:#fff">
-            <ul class="navbar-nav nav-justified w-100 d-flex justify-content-around align-items-center">
+            <ul class="navbar-nav nav-justified w-100 justify-content-around">
                 <li class="nav-item">
                     <a href="<?= base_url() ?>" class="nav-link link-light"><i class="bi bi-house-door-fill fw-bold fs-2 text-danger"></i></a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item me-5">
                     <a href="<?= base_url(); ?>cart" class="nav-link link-light a_cart_link_0">
                         <i class="bi bi-cart-fill fw-bold fs-2 position-absolute text-danger">
                             <div id="cartItem_0">
                                 <i class="bi bi-app-indicator position-absolute top-0 start-100 translate-middle text-danger mx-2 mt-2"></i>
-                                <span id="cartItem_1" class="position-absolute top-0 start-100 translate-middle badge badge-initial rounded-pill text-danger fw-bold mt-2 mx-2" style="font-size: 0.75rem;"><?= session()->get('countCart'); ?></span>
+                                <span id="cartItem_1" class="position-absolute top-0 start-100 translate-middle badge badge-initial rounded-pill text-danger fw-bold mx-2 mt-2" style="font-size: 0.75rem;"><?= session()->get('countCart'); ?></span>
                             </div>
                         </i>
                     </a>
