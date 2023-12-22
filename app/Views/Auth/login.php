a8a6f7babc6c47a07ac0aa505a37cb3aa3d2e722
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-12-22 17:06:51 +0700

    update
    - Disable login google

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -45,7 +45,7 @@ $error = session('error'); ?>
                         <p class="text-center fw-medium mb-0"><?= lang('Text.atau') ?></p>
                     </div>
 
-                    <a onclick="buttonGoogle()" href="<?= base_url('oauth/glogin'); ?>" class="button justify-content-center align-items-center my-3" style="border: 1px solid #000000;">
+                    <!-- <a onclick="buttonGoogle()" href="<?= base_url('oauth/glogin'); ?>" class="button justify-content-center align-items-center my-3" style="border: 1px solid #000000;">
                         <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262">
                             <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.690 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
                             <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
@@ -53,7 +53,7 @@ $error = session('error'); ?>
                             <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
                         </svg>
                         <?= lang('Text.login_google') ?>
-                    </a>
+                    </a> -->
 
                     <!-- <a id="btn-login-apple" class="button justify-content-center align-items-center my-3" style="border: 1px solid #000000;">
                         <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 300 320">
