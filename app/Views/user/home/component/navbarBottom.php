47879dd1bad5e86ee179cecb5999c1ade50dd1d4
ramadhani <rizqiramadhani37@gmail.com>
2023-12-22 10:26:34 +0700

    trouble navabr aprt 3 done testing

--- app/Views/user/home/component/navbarBottom.php
+++ app/Views/user/home/component/navbarBottom.php
@@ -7,20 +7,19 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
 
 
 <style>
-                nav.navbar-bottom {
-  border-radius: 0 !important;
-    border-top-left-radius: 15px !important;
-  border-top-right-radius: 15px !important;
-  border-bottom: 0 !important;
+    nav.navbar-bottom {
+        border-radius: 0 !important;
+        border-top-left-radius: 15px !important;
+        border-top-right-radius: 15px !important;
+        border-bottom: 0 !important;
 
 
-}
-
+    }
 </style>
 <!-- Footer Mobile -->
 <?php if ($isMobile) : ?>
     <div id="mobileContent">
-        <nav class="navbar navbar-expand fixed-bottom shadow-sm navbar-bottom bg-danger rounded-top-4" style="height: 55px; background-color:#fff">
+        <nav class="navbar navbar-expand fixed-bottom shadow-sm navbar-bottom rounded-top-4" style="height: 55px; background-color:#fff">
             <ul class="navbar-nav nav-justified w-100 justify-content-around">
                 <li class="nav-item">
                     <a href="<?= base_url() ?>" class="nav-link link-light"><i class="bi bi-house-door-fill fw-bold fs-2 text-danger"></i></a>
@@ -160,7 +159,7 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
             </div>
         </footer>
         <style>
-/* navabr bottom */
+            /* navabr bottom */
 
             .footer {
                 font-size: 14px;
