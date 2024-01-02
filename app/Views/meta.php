27fd7ec938b22d7512ee2bb1ca647a9058418c4f
kikuKeii <miftakhuddinfalaki@gmail.com>
2024-01-02 09:44:46 +0700

    update

---
+++ app/Views/meta.php
@@ -0,0 +1,22 @@
+<meta name="keywords" content="E-commerce, Online Shop, Ssayomart, Ssayomart.com, Shopping, Nori, Nori Go, Snack Korea">
+
+<meta name="title" content="<?= (isset($title)) ? $title : ''; ?>" />
+<meta property="type" content="website" />
+<meta property="url" content="<?= site_url(uri_string()); ?>" />
+<meta property="title" content="<?= (isset($title)) ? $title : ''; ?>" />
+<meta property="description" content="<?= (isset($description_meta)) ? $description_meta : 'Ssayomart memprioritaskan kualitas produk, keberlanjutan, dan kepuasan pelanggan.'; ?>" />
+<meta property="image" content="<?= (isset($img_meta)) ? $img_meta : base_url('asstes/img/logo.png'); ?>" />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="<?= site_url(uri_string()); ?>" />
+<meta property="og:title" content="<?= (isset($title)) ? $title : ''; ?>" />
+<meta property="og:description" content="<?= (isset($description_meta)) ? $description_meta : 'Ssayomart memprioritaskan kualitas produk, keberlanjutan, dan kepuasan pelanggan.'; ?>" />
+<meta property="og:image" content="<?= (isset($img_meta)) ? $img_meta : base_url('asstes/img/logo.png'); ?>" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="<?= (isset($description_meta)) ? $description_meta : 'Ssayomart memprioritaskan kualitas produk, keberlanjutan, dan kepuasan pelanggan.'; ?>" />
+<meta property="twitter:url" content="<?= site_url(uri_string()); ?>" />
+<meta property="twitter:title" content="<?= (isset($title)) ? $title : ''; ?>" />
+<meta property="twitter:description" content="<?= (isset($description_meta)) ? $description_meta : 'Ssayomart memprioritaskan kualitas produk, keberlanjutan, dan kepuasan pelanggan.'; ?>" />
+<meta property="twitter:image" content="<?= (isset($img_meta)) ? $img_meta : base_url('asstes/img/logo.png'); ?>" />
