61516cfcaf40cf7ae865e62c90ac1e7ad5f319ea
AnaMarisa <ana.marisa.mf@gmail.com>
2024-01-04 16:50:34 +0700

    revisi

--- app/Views/dashboard/userManagement/deleteRequest.php
+++ app/Views/dashboard/userManagement/deleteRequest.php
@@ -8,7 +8,7 @@
     <div class="card-header border-0 py-3 d-flex">
         <h6 class="m-0 font-weight-bold text-danger">List Request Penghapusan Akun</h6>
     </div>
-    <div class="card-body ">
+    <div class="card-body">
         <div class="row">
             <div class="col">
                 <div class="table-responsive">
@@ -30,56 +30,56 @@
                                 });
                                 $user = reset($user); // Get the first (and only) element of the filtered array
                                 ?>
+
                                 <tr>
                                     <td><?= $user['fullname']; ?></td>
                                     <td><?= $user['username']; ?></td>
                                     <td><?= $del['alasan']; ?></td>
                                     <td class="text-center">
-                                        <button type="button" class="btn position-relative" data-bs-toggle="modal" data-bs-target="#confirmationDelete">
+                                        <button type="button" class="btn position-relative" data-bs-toggle="modal" data-bs-target="#confirmationDelete<?= $del['id_request_delete'] ?>">
                                             <i class="bi bi-trash-fill fs-5 text-danger"></i>
                                         </button>
                                     </td>
                                 </tr>
 
+                                <!-- Modal Confirmation Delete -->
+                                <div class="modal fade" id="confirmationDelete<?= $del['id_request_delete'] ?>" tabindex="-1" aria-labelledby="confirmationDeleteLabel<?= $del['id_request_delete'] ?>" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="confirmationDeleteLabel<?= $del['id_request_delete'] ?>">Konfirmasi Penghapusan Akun</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="alert alert-danger">
+                                                    <div class="col-auto text-center mb-2" style="font-size:50px;">
+                                                        <i class="bi bi-exclamation-triangle-fill"></i>
+                                                    </div>
+                                                    <div class="col text-center">
+                                                        <p class="text-uppercase"><strong>Penghapusan akun bersifat permanen.</strong></p>
+                                                        <p>Apakah anda yakin untuk melakukan penghapusan akun?</p>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                                    <form action="<?= base_url('dashboard/user-management/delete-account/delete/' . $del['id_request_delete']) ?>" method="post" enctype="multipart/form-data">
+                                                        <?= csrf_field() ?>
+                                                        <input type="hidden" name="id" value="<?= $del['id_request_delete']; ?>">
+                                                        <button type="submit" class="btn btn-danger">Ya, Yakin</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
-
-                    <!-- Modal Confirmation Delete -->
-                    <div class="modal fade" id="confirmationDelete" tabindex="-1" aria-labelledby="confirmationDelete" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi Penghapusan Akun</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-
-                                    <div class="alert alert-danger">
-                                        <div class="col-auto text-center mb-2" style="font-size:50px;">
-                                            <i class="bi bi-exclamation-triangle-fill"></i>
-                                        </div>
-                                        <div class="col text-center">
-                                            <p class="text-uppercase"><strong>Penghapusan akun bersifat permanen.</strong></p>
-                                            <p>Apakah anda yakin untuk melakukan penghapusan akun?</p>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                        <form action="<?= base_url() ?>dashboard/user-management/delete-account/delete/<?= $del['id_request_delete'] ?>" method="post" enctype="multipart/form-data">
-                                            <?= csrf_field() ?>
-                                            <button type="submit" class="btn btn-danger">Ya, Yakin</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
     </div>
+
 </div>
 
 <script>
