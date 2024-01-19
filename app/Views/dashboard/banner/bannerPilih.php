8722f19444e345946cea2f09a7de4cac1716a3ed
singgihseptiann <septiansinggih93@gmail.com>
2024-01-19 13:49:10 +0700

    add animasi di bannerPilih & managementFetching

--- app/Views/dashboard/banner/bannerPilih.php
+++ app/Views/dashboard/banner/bannerPilih.php
@@ -9,10 +9,10 @@
 <div class="row">
     <div class="col-md-6 col-lg-3 mb-4">
         <div class="card border-0 shadow-sm border-left-danger h-100">
-            <a href="<?= base_url() ?>dashboard/banner/tambah-banner" target="__blank" class="text-decoration-none">
+            <a href="<?= base_url() ?>dashboard/banner/tambah-banner" target="_blank" class="text-decoration-none icon-house">
                 <div class="card-body d-flex align-items-center justify-content-center">
                     <div class="text-center">
-                        <i class="bi bi-house-fill fs-2 text-secondary"></i>
+                        <i class="bi bi-house-fill fs-2 text-secondary icon-house"></i>
                         <h6 class="mt-2 text-secondary">Banner Homepage</h6>
                     </div>
                 </div>
@@ -22,10 +22,10 @@
 
     <div class="col-md-6 col-lg-3 mb-4">
         <div class="card border-0 shadow-sm border-left-danger h-100">
-            <a href="<?= base_url() ?>dashboard/banner/ads-konten-banner" target="__blank" class="text-decoration-none">
+            <a href="<?= base_url() ?>dashboard/banner/ads-konten-banner" target="_blank" class="text-decoration-none icon-artikel">
                 <div class="card-body d-flex align-items-center justify-content-center">
                     <div class="text-center">
-                        <i class="bi bi-newspaper fs-2 text-secondary"></i>
+                        <i class="bi bi-newspaper fs-2 text-secondary icon-artikel"></i>
                         <h6 class="mt-2 text-secondary">Banner Artikel / Blog Konten</h6>
                     </div>
                 </div>
@@ -35,10 +35,10 @@
 
     <div class="col-md-6 col-lg-3 mb-4">
         <div class="card border-0 shadow-sm border-left-danger h-100">
-            <a href="<?= base_url() ?>dashboard/banner/pop-up-banner" target="__blank" class="text-decoration-none">
+            <a href="<?= base_url() ?>dashboard/banner/pop-up-banner" target="_blank" class="text-decoration-none icon-collection">
                 <div class="card-body d-flex align-items-center justify-content-center">
                     <div class="text-center">
-                        <i class="bi bi-collection-fill fs-2 text-secondary"></i>
+                        <i class="bi bi-collection-fill fs-2 text-secondary icon-collection"></i>
                         <h6 class="mt-2 text-secondary">Pop Up Homepage</h6>
                     </div>
                 </div>
@@ -47,11 +47,11 @@
     </div>
 
     <div class="col-md-6 col-lg-3 mb-4">
-        <div class="card border-0 shadow-sm border-left-danger h-100">
-            <a href="<?= base_url() ?>dashboard/banner/promotion-banner" target="__blank" class="text-decoration-none">
+        <div class="card card-banner border-0 shadow-sm border-left-danger h-100">
+            <a href="<?= base_url() ?>dashboard/banner/promotion-banner" target="_blank" class="text-decoration-none icon-promosi">
                 <div class="card-body d-flex align-items-center justify-content-center">
                     <div class="text-center">
-                        <i class="bi bi-percent fs-2 text-secondary"></i>
+                        <i class="bi bi-percent fs-2 text-secondary icon-promosi"></i>
                         <h6 class="mt-2 text-secondary">Banner Promosi</h6>
                     </div>
                 </div>
@@ -60,5 +60,66 @@
     </div>
 </div>
 
+<style>
+.icon-house,
+.icon-artikel,
+.icon-collection,
+.icon-promosi {
+    animation: customAni 2s ease 0s 1 normal none;
+    transform-origin: center bottom;
+}
+.icon-house:hover,
+.icon-artikel:hover,
+.icon-collection:hover,
+.icon-promosi:hover {
+    transform: scale(1.1); 
+    transition: transform 0.3s ease; 
+}
+@keyframes customAni {
+    0% {
+        animation-timing-function: ease-in;
+        opacity: 1;
+        transform: translateY(-20px);
+    }
+
+    24% {
+        opacity: 1;
+    }
+
+    40% {
+        animation-timing-function: ease-in;
+        transform: translateY(-10px);
+    }
+
+    65% {
+        animation-timing-function: ease-in;
+        transform: translateY(-5px);
+    }
+
+    82% {
+        animation-timing-function: ease-in;
+        transform: translateY(-3px);
+    }
+
+    93% {
+        animation-timing-function: ease-in;
+        transform: translateY(-2px);
+    }
+
+    25%,
+    55%,
+    75%,
+    87% {
+        animation-timing-function: ease-out;
+        transform: translateY(0px);
+    }
+
+    100% {
+        animation-timing-function: ease-out;
+        opacity: 1;
+        transform: translateY(0px);
+    }
+}
+</style>
 
 <?= $this->endSection(); ?>
--- app/Views/dashboard/produk/produkContent/managementFetching.php
+++ app/Views/dashboard/produk/produkContent/managementFetching.php
@@ -9,7 +9,7 @@
 <div class="row">
     <div class="col-md-6 col-lg-3 mb-4">
         <div class="card border-0 shadow-sm border-left-danger h-100">
-            <a href="<?= base_url() ?>dashboard/produk/pilih-produk-rekomendasi" target="__blank" class="text-decoration-none">
+            <a href="<?= base_url() ?>dashboard/produk/pilih-produk-rekomendasi" target="__blank" class="text-decoration-none icon-bookmark">
                 <div class="card-body d-flex align-items-center justify-content-center">
                     <div class="text-center">
                         <i class="bi bi-bookmark-star fs-2 text-secondary"></i>
@@ -22,7 +22,7 @@
 
     <div class="col-md-6 col-lg-3 mb-4">
         <div class="card border-0 shadow-sm border-left-danger h-100">
-            <a href="<?= base_url() ?>dashboard/produk/pilih-produk-terbaru" target="__blank" class="text-decoration-none">
+            <a href="<?= base_url() ?>dashboard/produk/pilih-produk-terbaru" target="__blank" class="text-decoration-none icon-grid">
                 <div class="card-body d-flex align-items-center justify-content-center">
                     <div class="text-center">
                         <i class="bi bi-grid-3x3-gap fs-2 text-secondary"></i>
@@ -33,5 +33,61 @@
         </div>
     </div>
 </div>
+<style>
+.icon-bookmark,
+.icon-grid {
+    animation: customAni 2s ease 0s 1 normal none;
+    transform-origin: center bottom;
+}
+.icon-bookmark:hover,
+.icon-grid:hover {
+    transform: scale(1.1); 
+    transition: transform 0.3s ease; 
+}
+@keyframes customAni {
+    0% {
+        animation-timing-function: ease-in;
+        opacity: 1;
+        transform: translateY(-20px);
+    }
 
+    24% {
+        opacity: 1;
+    }
+
+    40% {
+        animation-timing-function: ease-in;
+        transform: translateY(-10px);
+    }
+
+    65% {
+        animation-timing-function: ease-in;
+        transform: translateY(-5px);
+    }
+
+    82% {
+        animation-timing-function: ease-in;
+        transform: translateY(-3px);
+    }
+
+    93% {
+        animation-timing-function: ease-in;
+        transform: translateY(-2px);
+    }
+
+    25%,
+    55%,
+    75%,
+    87% {
+        animation-timing-function: ease-out;
+        transform: translateY(0px);
+    }
+
+    100% {
+        animation-timing-function: ease-out;
+        opacity: 1;
+        transform: translateY(0px);
+    }
+}
+</style>
 <?= $this->endSection(); ?>
