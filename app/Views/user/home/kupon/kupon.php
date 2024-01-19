320d855a1fe83be156f9963ac64bd6e6192005bc
Gilang Aditia <gaditia744@gmail.com>
2024-01-19 09:59:11 +0700

    update

--- app/Views/user/home/kupon/kupon.php
+++ app/Views/user/home/kupon/kupon.php
@@ -92,30 +92,28 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
     <!-- cupon end view mobile -->
 
     <!-- view Desktop -->
-    <div id="desktopContent" style="margin-top: 100px;">
-        <div class="container py-5">
-            <div class="col-12 d-flex justify-content-center">
-                <nav aria-label="breadcrumb" class="rounded-3 p-2">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item">
-                            <h2 class="mb-0 fw-bold"><i class="text-danger bi bi-box2-heart"></i> <?= $title; ?></h2>
-                            <hr class="mb-3 border-danger" style="border-width: 5px;">
-                        </li>
-                    </ol>
-                </nav>
-            </div>
-            <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div id="desktopContent" style="margin-top: 150px;">
+        <div class="container justify-content-center align-items-center py-5">
+            <div class="row">
                 <?php foreach ($kupon_model as $km) : ?>
-                    <div class="col pb-3">
-                        <div class="card border-0 shadow-sm bg-white">
-                            <img src="<?= base_url() ?>assets/img/promo/Clipped.png" class="card-img-top border-0 img-fluid img-thumbnail rounded-4" alt="...">
-                            <div class="card-body border-0 bg-white text-center ">
-                                <p class="fs-5"><?= $km['nama']; ?></p>
-                                <p class="text-secondary fs-6"><?= $km['kode']; ?></p>
-                                <p class="text-secondary fs-6">Kupon Tersedia : <?= $km['available_kupon']; ?></p>
+                    <div class="col-sm-6 mb-3">
+                        <div class="coupon bg-white rounded d-flex justify-content-between shadow-sm">
+                            <div class="kiri p-2">
+                                <div class="icon-container">
+                                    <img src="<?= base_url() ?>assets/img/promo/default.png" alt="..." width="75" class="rounded" />
+                                </div>
                             </div>
-                            <div class="card-footer bg-light border-0 shadow-sm text-body-secondary d-flex justify-content-center">
-                                <a href="<?= base_url() ?>" class="btn border-0 btn-danger">Belanja Sekarang</a>
+                            <div class="deskripsi" style="width: 100%; margin-left:20px; text-align:start;">
+                                <div class="tengah py-1 me-5">
+                                    <span class="badge text-bg-success" style="font-size: 10px;"><?= $km['kode']; ?></span>
+                                    <h3 class="lead mt-2 d-grid" style="font-size: 9px;"><?= $km['nama']; ?></h3>
+                                    <p class="text-secondary mb-0" style="font-size: 10px;">Kupon Tersedia : <?= $km['available_kupon']; ?></p>
+                                </div>
+                            </div>
+                            <div class="kanan">
+                                <div class="info m-3 d-flex flex-column justify-content-between">
+                                    <a href="<?= base_url() ?>" class="btn btn-sm btn-outline-danger btn-block mt-3" style="font-size: 12px;">Belanja</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -123,6 +121,7 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
             </div>
         </div>
     </div>
+
     <!-- End Footer Desktop -->
 
 <?php endif; ?>
