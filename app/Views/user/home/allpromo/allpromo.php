618f94d04d16f3bb6df43a8f3f25970c66310581
Gilang Aditia <gaditia744@gmail.com>
2024-03-06 13:50:43 +0700

    update

--- app/Views/user/home/allpromo/allpromo.php
+++ app/Views/user/home/allpromo/allpromo.php
@@ -2,7 +2,7 @@
 <?= $this->section('page-content') ?>
 
 <div class="container">
-    <div class="card mb-3">
+    <div class="card mb-3 shadow-sm border-0">
         <img src="<?= base_url() ?>assets/img/promo/1701760476_754480444146dd2fdb1f.jpg" class="card-img-top" alt="">
         <div class="card-body">
             <h5 class="card-title fw-bold m-0">Promo asik-asik</h5>
@@ -10,7 +10,7 @@
             <p class="card-text"><small class="text-body-secondary">diupload 6 maret</small></p>
         </div>
     </div>
-    <div class="card mb-3">
+    <div class="card mb-3 shadow-sm border-0">
         <img src="<?= base_url() ?>assets/img/promo/1701760532_f63f4d88028cfe9482fa.jpg" class="card-img-top" alt="">
         <div class="card-body">
             <h5 class="card-title fw-bold m-0">Promo asik-asik</h5>
@@ -18,7 +18,7 @@
             <p class="card-text"><small class="text-body-secondary">diupload 6 maret</small></p>
         </div>
     </div>
-    <div class="card mb-3">
+    <div class="card mb-3 shadow-sm border-0">
         <img src="<?= base_url() ?>assets/img/promo/1701760532_f63f4d88028cfe9482fa.jpg" class="card-img-top" alt="">
         <div class="card-body">
             <h5 class="card-title fw-bold m-0">Promo asik-asik</h5>
