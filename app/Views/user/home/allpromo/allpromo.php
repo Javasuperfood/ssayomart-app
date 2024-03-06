fa773694557fc8de54c91f2d1c72ba73ba653506
Gilang Aditia <gaditia744@gmail.com>
2024-03-06 13:47:54 +0700

    update

--- app/Views/user/home/allpromo/allpromo.php
+++ app/Views/user/home/allpromo/allpromo.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="card mb-3">
         <img src="<?= base_url() ?>assets/img/promo/1701760476_754480444146dd2fdb1f.jpg" class="card-img-top" alt="">
-        <div class="card-body m-0">
-            <h5 class="card-title fw-bold">Promo asik-asik</h5>
+        <div class="card-body">
+            <h5 class="card-title fw-bold m-0">Promo asik-asik</h5>
             <p class="card-text">ini adalah promo murah yang ada di ssayomart</p>
             <p class="card-text"><small class="text-body-secondary">diupload 6 maret</small></p>
         </div>
@@ -13,7 +13,7 @@
     <div class="card mb-3">
         <img src="<?= base_url() ?>assets/img/promo/1701760532_f63f4d88028cfe9482fa.jpg" class="card-img-top" alt="">
         <div class="card-body">
-            <h5 class="card-title fw-bold">Promo asik-asik</h5>
+            <h5 class="card-title fw-bold m-0">Promo asik-asik</h5>
             <p class="card-text">ini adalah promo murah yang ada di ssayomart</p>
             <p class="card-text"><small class="text-body-secondary">diupload 6 maret</small></p>
         </div>
@@ -21,7 +21,7 @@
     <div class="card mb-3">
         <img src="<?= base_url() ?>assets/img/promo/1701760532_f63f4d88028cfe9482fa.jpg" class="card-img-top" alt="">
         <div class="card-body">
-            <h5 class="card-title fw-bold">Promo asik-asik</h5>
+            <h5 class="card-title fw-bold m-0">Promo asik-asik</h5>
             <p class="card-text">ini adalah promo murah yang ada di ssayomart</p>
             <p class="card-text"><small class="text-body-secondary">diupload 6 maret</small></p>
         </div>
