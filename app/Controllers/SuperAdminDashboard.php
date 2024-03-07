8848d876a7f34ffa53188e38a554e48eb40fae52
Gilang Aditia <gaditia744@gmail.com>
2024-03-07 11:15:11 +0700

    update

--- app/Controllers/SuperAdminDashboard.php
+++ app/Controllers/SuperAdminDashboard.php
@@ -42,4 +42,39 @@ class SuperAdminDashboard extends BaseController
 
         return view('dashboard/superadmin/dashboard', $data);
     }
+
+
+    public function detailinv()
+    {
+        $checkoutModel = new CheckoutModel();
+        $checkoutProdModel = new CheckoutProdukModel();
+        $adminTokoModel = new AdminTokoModel();
+        $perPage = 10;
+
+        $startDate = $this->request->getVar('startDate');
+        $endDate = $this->request->getVar('endDate');
+
+        // Tambahkan fungsi untuk mendapatkan daftar cabang
+        $branches = $checkoutModel->getBranches();
+        $adminToko = $adminTokoModel->getAdminToko(user_id());
+
+        $getSuperAdminReport = $checkoutModel->getSuperAdminReport($perPage, $startDate, $endDate);
+        foreach ($getSuperAdminReport as $key => $c) {
+            $getSuperAdminReport[$key]['produk'] = $checkoutProdModel->getProdukByIdCheckout($c['id_checkout']);
+        }
+        $currentPage = $this->request->getVar('page_checkout') ? $this->request->getVar('page_checkout') : 1;
+
+        $data = [
+            'getSuperAdminReport' => $getSuperAdminReport,
+            'pager' => $checkoutModel->pager,
+            'iterasi' => ($currentPage - 1) * $perPage + 1,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'market' => $adminToko,
+            'branches' => $branches, // Sertakan daftar cabang
+        ];
+        $data['penjualan'] = $checkoutModel->getSalesReportByBranch(1);
+
+        return view('dashboard/superadmin/detailInv', $data);
+    }
 }
