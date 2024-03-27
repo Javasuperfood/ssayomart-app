c1f05cd58584e4f618ad1da40dd129094807c341
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2024-03-27 11:47:09 +0700

    Update Midtrans.php

--- app/Config/Midtrans.php
+++ app/Config/Midtrans.php
@@ -14,14 +14,14 @@ class Midtrans extends BaseConfig
     public $urlMidtrans;
     public function __construct()
     {
-        $isP = getenv('MIDTRANS_MODE');
-        // $isP = getenv('CI_ENVIRONMENT');
+        // $isP = getenv('MIDTRANS_MODE');
+        $isP = getenv('CI_ENVIRONMENT');
         if ($isP == 'production') {
             $this->isProduction = true;
             $this->serverKey = 'Mid-server-aLZoDj1cUy1D4vxBdOwA3E9e';
             $this->clientKey = 'Mid-client-5aUc7KzYPl1l8Lpj';
             $this->urlMidtrans = 'https://app.midtrans.com/snap/snap.js';
-        } else {
+        } elseif ($isP == 'development') {
             $this->isProduction = false;
             $this->serverKey = 'SB-Mid-server-9ya8fzSZH6K-HJyvn5kmuG-8';
             $this->clientKey = 'SB-Mid-client-T_uUJ6ot5ry3BrGh';
