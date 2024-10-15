a9575e6f425a5aa9f32a52a5bc87f03cf2aa51e3
Gilang Aditia <gaditia744@gmail.com>
2024-10-15 10:32:30 +0700

    update view

--- app/Views/user/home/cart/cart2.php
+++ app/Views/user/home/cart/cart2.php
@@ -97,9 +97,9 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                                     <!-- Fungsi Multi Language -->
                                     <!-- <p class="fw-bold card-text text-secondary float-start nama-pesanan" style="font-size: 12px; margin: 0;"><?= substr($p[$kolomNama] . '(' . $p['value_item'] . ')', 0, 30); ?></p> -->
                                     <?php if ($p['required_quantity']) : ?>
-                                        <p class="text-secondary text-pixel float-end mb-2" style="width:80%;font-size:11px; margin: 0;"><?= substr($p['title'] . '(' . $p['value_item'] . ')', 0,); ?></p>
+                                        <p class="text-secondary text-pixel mb-2" style="font-size: 12px; margin: 0;"><?= substr($p['title'] . '(' . $p['value_item'] . ')', 0, 30); ?></p>
                                     <?php else : ?>
-                                        <p class="text-secondary text-pixel float-end mb-2" style="width:80%;font-size:11px; margin: 0;"><?= substr($p['nama'] . '(' . $p['value_item'] . ')', 0,); ?></p>
+                                        <p class="text-secondary text-pixel mb-2" style="font-size: 12px; margin: 0;"><?= substr($p['nama'] . '(' . $p['value_item'] . ')', 0, 30); ?></p>
                                     <?php endif; ?>
                                     <div class="input-group grup-masukan button-group" style="margin-top:5px; right: 45%;">
                                         <button class="btn-sm btn btn-outline-danger btn-dash rounded-circle" style="width: 20px; height:22px;" type="button" onClick='decreaseCount(<?= $p['id_cart_produk']; ?>, event, this, <?= $p['harga_item']; ?>, <?= isset($p['required_quantity']) ? $p['required_quantity'] : 'null'; ?>)'><i class="bi bi-dash"></i></button>
