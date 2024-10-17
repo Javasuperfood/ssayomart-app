c32db741040e6f615b9f3f787f9a30a9246e5f2c
Gilang Aditia <gaditia744@gmail.com>
2024-10-17 11:18:08 +0700

    update bug fixing old pass

--- app/Views/user/home/setting/changePassword/oldPassword.php
+++ app/Views/user/home/setting/changePassword/oldPassword.php
@@ -34,23 +34,7 @@ $isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Table
                         </div>
                         <div class="invalid-feedback"><?= validation_show_error('oldPass'); ?></div>
                     </div>
-
-                    <!-- <div class="col-12 mt-2">
-                <label for="label" class="form-label mb-0 mx-1" style="font-size: 12px;">Masukan Password yang lama <span class="text-danger" style="font-size: 13px;"> *</span></label>
-                <input type="password" name="oldPass" class="form-control p-3 form-control-lg shadow-sm <?= (validation_show_error('oldPass')) ? 'is-invalid' : 'border-0'; ?>" id="oldPass" placeholder="Password" style="font-size: 11px;">
-                <div class="invalid-feedback"><?= validation_show_error('oldPass'); ?></div>
-            </div> -->
                 </div>
-                <!-- <div class="col-12 mt-2">
-            <label for="label" class="form-label mb-0 mx-1" style="font-size: 12px;">Masukan Password yang baru <span class="text-danger" style="font-size: 13px;"> *</span></label>
-            <input type="password" name="newPass" class="form-control p-3 form-control-lg shadow-sm <?= (validation_show_error('newPass')) ? 'is-invalid' : 'border-0'; ?>" id="newPass" placeholder="Password baru" style="font-size: 11px;">
-            <div class="invalid-feedback"><?= validation_show_error('newPass'); ?></div>
-        </div>
-        <div class="col-12 mt-2">
-            <label for="label" class="form-label mb-0 mx-1" style="font-size: 12px;">Verifikasi Password yang baru <span class="text-danger" style="font-size: 13px;"> *</span></label>
-            <input type="password" name="reNewPass" class="form-control p-3 form-control-lg shadow-sm <?= (validation_show_error('reNewPass')) ? 'is-invalid' : 'border-0'; ?>" id="newPass" placeholder="Verifikasi Password" style="font-size: 11px;">
-            <div class="invalid-feedback"><?= validation_show_error('reNewPass'); ?></div>
-        </div> -->
 
                 <div class="col-12 mt-2">
                     <label for="label" class="form-label mb-0 mx-1" style="font-size: 12px;">
