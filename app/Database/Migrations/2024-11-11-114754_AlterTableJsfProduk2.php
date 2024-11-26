8fe486b7e55466e8a65052d9ddb88b714c3e01c6
anamarisa <ana.marisa.mf@gmail.com>
2024-11-26 18:36:22 +0700

    sort by brand

--- app/Database/Migrations/2024-11-11-114754_AlterTableJsfProduk2.php
+++ app/Database/Migrations/2024-11-11-114754_AlterTableJsfProduk2.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class AlterTableJsfProduk extends Migration
+class AlterTableJsfProduk2 extends Migration
 {
     public function up()
     {
